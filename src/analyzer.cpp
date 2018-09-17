ee7e955155fce44f8d5b82012aac9947cd396466
hatsusato <knightspm5@gmail.com>
2018-09-17 12:22:16 +0900

    Add stacksafe::Analyzer class

--- src/analyzer.cpp
+++ src/analyzer.cpp
@@ -1,1 +1,11 @@
 #include "analyzer.hpp"
+
+namespace stacksafe {
+  char Analyzer::ID = 0;
+  Analyzer::Analyzer()
+    : llvm::FunctionPass(ID)
+  {}
+  bool Analyzer::runOnFunction(llvm::Function &F) {
+    return false;
+  }
+}
--- src/analyzer.hpp
+++ src/analyzer.hpp
@@ -1,3 +1,15 @@
 #ifndef INCLUDE_GUARD_24D0B7C2_7662_4CA7_9DAE_CFC9EA3B0EA0
 #define INCLUDE_GUARD_24D0B7C2_7662_4CA7_9DAE_CFC9EA3B0EA0
+
+#include "llvm/Pass.h"
+
+namespace stacksafe {
+  class Analyzer : public llvm::FunctionPass {
+  public:
+    Analyzer();
+    static char ID;
+    bool runOnFunction(llvm::Function &F) override;
+  };
+}
+
 #endif//INCLUDE_GUARD_24D0B7C2_7662_4CA7_9DAE_CFC9EA3B0EA0
