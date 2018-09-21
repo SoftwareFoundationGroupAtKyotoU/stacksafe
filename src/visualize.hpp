50fa441158e7f7583600281eef02f0f227564eca
hatsusato <knightspm5@gmail.com>
2018-09-21 15:48:42 +0900

    Add ClassNameVisitor

--- src/visualize.hpp
+++ src/visualize.hpp
@@ -1,3 +1,23 @@
 #ifndef INCLUDE_GUARD_30B970DD_A9A2_4C3C_B2A3_482271B1A2C5
 #define INCLUDE_GUARD_30B970DD_A9A2_4C3C_B2A3_482271B1A2C5
+
+#include <llvm/IR/InstVisitor.h>
+
+namespace llvm {
+#define HANDLE_INST(NUM, OPCODE, CLASS) class CLASS;
+#include <llvm/IR/Instruction.def>
+#undef HANDLE_INST
+}
+namespace stacksafe {
+  class ClassNameVisitor :
+    public llvm::InstVisitor<ClassNameVisitor, const char *> {
+    using RetTy = const char *;
+  public:
+#define HANDLE_INST(NUM, OPCODE, CLASS) \
+    RetTy visit##OPCODE(llvm::CLASS &I) { return #CLASS ": " #OPCODE; }
+#include <llvm/IR/Instruction.def>
+#undef HANDLE_INST
+  };
+}
+
 #endif//INCLUDE_GUARD_30B970DD_A9A2_4C3C_B2A3_482271B1A2C5
