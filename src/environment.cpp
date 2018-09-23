482fb6604713b3bf40c9037f80d24e45f1dc02c7
hatsusato <knightspm5@gmail.com>
2018-09-23 20:17:49 +0900

    Add LocationFactory

--- src/environment.cpp
+++ src/environment.cpp
@@ -1,1 +1,24 @@
 #include "environment.hpp"
+
+namespace stacksafe {
+  namespace {
+    constexpr char nullch = '\0';
+  }
+  char LocationFactory::outlive_ = nullch;
+  char LocationFactory::global_ = nullch;
+  char LocationFactory::undefined_ = nullch;
+  auto LocationFactory::getLocal() -> Location {
+    auto data = std::make_unique<char>(nullch);
+    local_.push_back(std::move(data));
+    return local_.back().get();
+  }
+  auto LocationFactory::getOutlive() -> Location {
+    return &outlive_;
+  }
+  auto LocationFactory::getGlobal() -> Location {
+    return &global_;
+  }
+  auto LocationFactory::getUndefined() -> Location {
+    return &undefined_;
+  }
+}
--- src/environment.hpp
+++ src/environment.hpp
@@ -1,3 +1,20 @@
 #ifndef INCLUDE_GUARD_52F60E3D_17C8_4A02_BEB6_64AF9019A2B4
 #define INCLUDE_GUARD_52F60E3D_17C8_4A02_BEB6_64AF9019A2B4
+
+#include <memory>
+#include <vector>
+
+namespace stacksafe {
+  class LocationFactory {
+    using Location = void *;
+    std::vector<std::unique_ptr<char>> local_;
+    static char outlive_, global_, undefined_;
+  public:
+    Location getLocal();
+    Location getOutlive();
+    Location getGlobal();
+    Location getUndefined();
+  };
+}
+
 #endif//INCLUDE_GUARD_52F60E3D_17C8_4A02_BEB6_64AF9019A2B4
