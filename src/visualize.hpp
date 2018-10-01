e36ea4ecd1a0a64d72aba261b2f2b72a38fc13a1
hatsusato <knightspm5@gmail.com>
2018-10-01 12:13:22 +0900

    Add wrap

--- src/visualize.hpp
+++ src/visualize.hpp
@@ -1,7 +1,9 @@
 #ifndef INCLUDE_GUARD_30B970DD_A9A2_4C3C_B2A3_482271B1A2C5
 #define INCLUDE_GUARD_30B970DD_A9A2_4C3C_B2A3_482271B1A2C5
 
+#include <functional>
 #include <llvm/IR/InstVisitor.h>
+#include <llvm/Support/raw_ostream.h>
 
 namespace llvm {
 #define HANDLE_INST(NUM, OPCODE, CLASS) class CLASS;
@@ -21,6 +23,10 @@ namespace stacksafe {
 #undef HANDLE_INST
   };
 
+  using ManipObj = std::function<void(llvm::raw_ostream &)>;
+  const auto wrap = [](const auto &x) -> ManipObj {
+    return [=](llvm::raw_ostream &O) { O << x; };
+  };
   template <class T>
   llvm::raw_ostream &operator<<(llvm::raw_ostream &O, const T &x) {
     x.print(O);
