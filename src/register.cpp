cf8537de7e74714fe9e96933d3cdaaa9b6ee0b8c
hatsusato <knightspm5@gmail.com>
2018-10-10 00:51:44 +0900

    Support hash for Register

--- src/register.cpp
+++ src/register.cpp
@@ -5,4 +5,12 @@ namespace stacksafe {
   Register::Register(llvm::Value &value)
     : reg_(&value)
   {}
+  std::size_t Register::hash() const {
+    return std::hash<void *>{}(reg_);
+  }
+}
+namespace std {
+  size_t hash<stacksafe::Register>::operator()(stacksafe::Register r) const {
+    return r.hash();
+  }
 }
--- src/register.hpp
+++ src/register.hpp
@@ -1,6 +1,8 @@
 #ifndef INCLUDE_GUARD_40FBF2CB_F11A_46FD_8C86_B65C80D3F664
 #define INCLUDE_GUARD_40FBF2CB_F11A_46FD_8C86_B65C80D3F664
 
+#include <functional>
+
 namespace llvm {
   class Value;
 }
@@ -9,6 +11,13 @@ namespace stacksafe {
     llvm::Value *reg_;
   public:
     Register(llvm::Value &value);
+    std::size_t hash() const;
+  };
+}
+namespace std {
+  template <>
+  struct hash<stacksafe::Register> {
+    size_t operator()(stacksafe::Register r) const;
   };
 }
 
