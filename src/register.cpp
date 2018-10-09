8757cb4fd419262c2dafb3c44144608e02deb0cd
hatsusato <knightspm5@gmail.com>
2018-10-10 00:47:11 +0900

    Add Register class

--- src/register.cpp
+++ src/register.cpp
@@ -1,1 +1,8 @@
 #include "register.hpp"
+#include <llvm/IR/Value.h>
+
+namespace stacksafe {
+  Register::Register(llvm::Value &value)
+    : reg_(&value)
+  {}
+}
--- src/register.hpp
+++ src/register.hpp
@@ -1,3 +1,15 @@
 #ifndef INCLUDE_GUARD_40FBF2CB_F11A_46FD_8C86_B65C80D3F664
 #define INCLUDE_GUARD_40FBF2CB_F11A_46FD_8C86_B65C80D3F664
+
+namespace llvm {
+  class Value;
+}
+namespace stacksafe {
+  class Register {
+    llvm::Value *reg_;
+  public:
+    Register(llvm::Value &value);
+  };
+}
+
 #endif//INCLUDE_GUARD_40FBF2CB_F11A_46FD_8C86_B65C80D3F664
