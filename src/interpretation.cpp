e012bc53eee1dbac4b0a0eb0bc5d4a9f3e06634c
hatsusato <knightspm5@gmail.com>
2019-03-05 14:48:21 +0900

    Add Interpretation class

--- src/interpretation.cpp
+++ src/interpretation.cpp
@@ -1,1 +1,25 @@
 #include "interpretation.hpp"
+#include "visualize.hpp"
+#include <llvm/IR/Function.h>
+#include <llvm/Support/raw_ostream.h>
+
+namespace stacksafe {
+  Interpretation::Interpretation(llvm::Function &F) {
+    Env empty(factory_);
+    for (auto &b : F.getBasicBlockList()) {
+      map_.emplace(&b, empty);
+    }
+    auto entry = &F.getEntryBlock();
+    todo_.push(entry);
+    for (auto &a: F.args()) {
+      if (auto reg = make_register(a)) {
+        if (!map_.at(entry).argument(*reg)) {
+          llvm::errs() << "Error: " << spaces(make_manip(*reg))
+                       << "is not an argument" << endl;
+        }
+      } else {
+        llvm::errs() << "Error: unreachable" << endl;
+      }
+    }
+  }
+}
--- src/interpretation.hpp
+++ src/interpretation.hpp
@@ -1,3 +1,24 @@
 #ifndef INCLUDE_GUARD_FBC5E6D0_9143_4FDB_9ED6_5F3DE4E64961
 #define INCLUDE_GUARD_FBC5E6D0_9143_4FDB_9ED6_5F3DE4E64961
+
+#include "environment.hpp"
+#include "location.hpp"
+#include <queue>
+#include <unordered_map>
+
+namespace llvm {
+  class BasicBlock;
+  class Function;
+}
+namespace stacksafe {
+  class Interpretation {
+    using BB = llvm::BasicBlock *;
+    LocationFactory factory_;
+    std::unordered_map<BB, Env> map_;
+    std::queue<BB> todo_;
+  public:
+    Interpretation(llvm::Function &F);
+  };
+}
+
 #endif//INCLUDE_GUARD_FBC5E6D0_9143_4FDB_9ED6_5F3DE4E64961
