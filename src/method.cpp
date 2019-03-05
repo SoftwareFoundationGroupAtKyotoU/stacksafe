04b8684cdcefbf0068674cf9096020a1eed55d22
hatsusato <knightspm5@gmail.com>
2019-03-05 15:24:38 +0900

    Add Method class

--- src/method.cpp
+++ src/method.cpp
@@ -1,1 +1,13 @@
 #include "method.hpp"
+
+namespace stacksafe {
+  Method::Method(Env &env)
+    : env_(env) {
+  }
+  bool Method::visit(llvm::Instruction &I) {
+    // auto ret = Base::visit(I);
+    llvm::outs() << I << endl;
+    llvm::outs() << env_;
+    return false;
+  }
+}
--- src/method.hpp
+++ src/method.hpp
@@ -1,3 +1,17 @@
 #ifndef INCLUDE_GUARD_6D412E24_64BA_4C27_BEA5_DB1CAFCC48BD
 #define INCLUDE_GUARD_6D412E24_64BA_4C27_BEA5_DB1CAFCC48BD
+
+#include "environment.hpp"
+#include <llvm/IR/InstVisitor.h>
+
+namespace stacksafe {
+  class Method : public llvm::InstVisitor<Method, bool> {
+    using Base = llvm::InstVisitor<Method, bool>;
+    Env &env_;
+  public:
+    explicit Method(Env &env);
+    bool visit(llvm::Instruction &I);
+  };
+}
+
 #endif//INCLUDE_GUARD_6D412E24_64BA_4C27_BEA5_DB1CAFCC48BD
