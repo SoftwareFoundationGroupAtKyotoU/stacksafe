607bd1eacc3cf95bd1ade12403f31b1565252231
hatsusato <knightspm5@gmail.com>
2019-05-28 16:00:35 +0900

    Format location

--- src/location.cpp
+++ src/location.cpp
@@ -4,55 +4,43 @@
 #include <string>
 
 namespace stacksafe {
-  const int Location::Global = -1;
-  const int Location::Outlive = 0;
-  const int Location::Local = 1;
-  Location::Location(int loc)
-    : loc_(loc)
-  {}
-  std::size_t Location::hash() const {
-    return std::hash<int>{}(loc_);
-  }
-  void Location::print(llvm::raw_ostream &O) const {
-    auto ascii =
-      [](int n) -> std::string {
-        switch (n) {
-        case Location::Global:
-          return "#_g";
-        case Location::Outlive:
-          return "#_o";
-        }
-        const int base = 26;
-        std::string s;
-        while (0 < n) {
-          const auto digit = (n - 1) % base;
-          s.append(1, 'a' + digit);
-          n = (n - digit - 1) / base;
-        }
-        s.append("#");
-        std::reverse(begin(s), end(s));
-        return s;
-      };
-    O << angles(make_manip(ascii(loc_)));
-  }
-  bool operator==(const Location &lhs, const Location &rhs) {
-    return lhs.hash() == rhs.hash();
-  }
+const int Location::Global = -1;
+const int Location::Outlive = 0;
+const int Location::Local = 1;
+Location::Location(int loc) : loc_(loc) {}
+std::size_t Location::hash() const { return std::hash<int>{}(loc_); }
+void Location::print(llvm::raw_ostream &O) const {
+  auto ascii = [](int n) -> std::string {
+    switch (n) {
+    case Location::Global:
+      return "#_g";
+    case Location::Outlive:
+      return "#_o";
+    }
+    const int base = 26;
+    std::string s;
+    while (0 < n) {
+      const auto digit = (n - 1) % base;
+      s.append(1, 'a' + digit);
+      n = (n - digit - 1) / base;
+    }
+    s.append("#");
+    std::reverse(begin(s), end(s));
+    return s;
+  };
+  O << angles(make_manip(ascii(loc_)));
+}
+bool operator==(const Location &lhs, const Location &rhs) {
+  return lhs.hash() == rhs.hash();
+}
 
-  LocationFactory::LocationFactory()
-    : current_(Location::Local) {}
-  Location LocationFactory::getGlobal() {
-    return Location(Location::Global);
-  }
-  Location LocationFactory::getOutlive() {
-    return Location(Location::Outlive);
-  }
-  Location LocationFactory::getLocal() {
-    return Location(current_++);
-  }
-}
+LocationFactory::LocationFactory() : current_(Location::Local) {}
+Location LocationFactory::getGlobal() { return Location(Location::Global); }
+Location LocationFactory::getOutlive() { return Location(Location::Outlive); }
+Location LocationFactory::getLocal() { return Location(current_++); }
+} // namespace stacksafe
 namespace std {
-  size_t hash<stacksafe::Location>::operator()(stacksafe::Location l) const {
-    return l.hash();
-  }
+size_t hash<stacksafe::Location>::operator()(stacksafe::Location l) const {
+  return l.hash();
 }
+} // namespace std
