d2cdddff09af871899be058fceb0fe410da8234b
hatsusato <knightspm5@gmail.com>
2019-08-23 16:36:01 +0900

    Add Value class

--- src/value.cpp
+++ src/value.cpp
@@ -1,1 +1,8 @@
 #include "value.hpp"
+#include <llvm/IR/Value.h>
+
+namespace stacksafe {
+namespace value {
+Value::Value(const llvm::Value& v) : val_{&v} {}
+}  // namespace value
+}  // namespace stacksafe
--- src/value.hpp
+++ src/value.hpp
@@ -1,6 +1,20 @@
 #ifndef INCLUDE_GUARD_838FCB87_603F_4E6D_A973_29F6EB0FD779
 #define INCLUDE_GUARD_838FCB87_603F_4E6D_A973_29F6EB0FD779
 
-namespace stacksafe {}
+namespace llvm {
+class Value;
+}
+namespace stacksafe {
+namespace value {
+
+class Value {
+  const llvm::Value* const val_;
+
+ public:
+  explicit Value(const llvm::Value& v);
+};
+
+}  // namespace value
+}  // namespace stacksafe
 
 #endif  // INCLUDE_GUARD_838FCB87_603F_4E6D_A973_29F6EB0FD779
