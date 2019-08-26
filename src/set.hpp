4d97d28e6a03263d1262e50613774b64263b0785
hatsusato <knightspm5@gmail.com>
2019-08-26 15:06:20 +0900

    Use set instead of unordered_set

--- src/set.hpp
+++ src/set.hpp
@@ -1,11 +1,11 @@
 #ifndef INCLUDE_GUARD_B43C0EC1_4338_47A3_AD6C_40B0DEF734B0
 #define INCLUDE_GUARD_B43C0EC1_4338_47A3_AD6C_40B0DEF734B0
 
-#include <unordered_set>
+#include <set>
 
 template <typename T>
-class Set : private std::unordered_set<T> {
-  using Base = std::unordered_set<T>;
+class Set : private std::set<T> {
+  using Base = std::set<T>;
 };
 
 #endif  // INCLUDE_GUARD_B43C0EC1_4338_47A3_AD6C_40B0DEF734B0
