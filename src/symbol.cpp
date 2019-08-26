9e49c98c47ff595ca58230517832c9cfa0d5e554
hatsusato <knightspm5@gmail.com>
2019-08-26 15:05:19 +0900

    Add Symbol::create

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -1,1 +1,5 @@
 #include "symbol.hpp"
+
+int Symbol::current_ = 0;
+Symbol::Symbol(int n) : num_{n} {}
+Symbol Symbol::create() { return Symbol{current_++}; }
--- src/symbol.hpp
+++ src/symbol.hpp
@@ -1,6 +1,13 @@
 #ifndef INCLUDE_GUARD_5A26F608_5BA4_4080_8212_72E9DC406772
 #define INCLUDE_GUARD_5A26F608_5BA4_4080_8212_72E9DC406772
 
-class Symbol {};
+class Symbol {
+  static int current_;
+  int num_;
+  explicit Symbol(int n);
+
+ public:
+  static Symbol create();
+};
 
 #endif  // INCLUDE_GUARD_5A26F608_5BA4_4080_8212_72E9DC406772
