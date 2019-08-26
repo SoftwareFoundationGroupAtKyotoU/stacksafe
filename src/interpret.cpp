53b6c867a5edb7efd53e03dc14f7bc5956a7c5cd
hatsusato <knightspm5@gmail.com>
2019-08-26 16:15:46 +0900

    Fix Interpret::visit

--- src/interpret.cpp
+++ src/interpret.cpp
@@ -4,4 +4,7 @@
 Interpret::Interpret(Abstraction &a) : abst_{a} {}
 void Interpret::visit(llvm::Function &f) { Base::visit(f); }
 void Interpret::visit(llvm::BasicBlock &b) { Base::visit(b); }
-void Interpret::visit(llvm::Instruction &i) { llvm::errs() << i << "\n"; }
+void Interpret::visit(llvm::Instruction &i) {
+  llvm::errs() << i << "\n";
+  Base::visit(i);
+}
