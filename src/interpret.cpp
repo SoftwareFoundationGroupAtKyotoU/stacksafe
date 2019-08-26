5a4c455a76f9fd52e4b6d380672f241331513657
hatsusato <knightspm5@gmail.com>
2019-08-26 16:16:53 +0900

    Add Interpret::visitAllocaInst

--- src/interpret.cpp
+++ src/interpret.cpp
@@ -1,5 +1,6 @@
 #include "interpret.hpp"
 #include "abstract.hpp"
+#include "register.hpp"
 
 Interpret::Interpret(Abstraction &a) : abst_{a} {}
 void Interpret::visit(llvm::Function &f) { Base::visit(f); }
@@ -7,4 +8,8 @@ void Interpret::visit(llvm::BasicBlock &b) { Base::visit(b); }
 void Interpret::visit(llvm::Instruction &i) {
   llvm::errs() << i << "\n";
   Base::visit(i);
+}
+void Interpret::visitAllocaInst(llvm::AllocaInst &i) {
+  Register r{i};
+  abst_.alloc(r);
 }
