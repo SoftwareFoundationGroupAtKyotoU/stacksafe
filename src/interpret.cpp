693066a0055461add136de9be5aeb98fcace809b
hatsusato <knightspm5@gmail.com>
2019-08-26 14:27:22 +0900

    Add Interpret::visit

--- src/interpret.cpp
+++ src/interpret.cpp
@@ -1,1 +1,5 @@
 #include "interpret.hpp"
+
+void Interpret::visit(llvm::Function &f) { Base::visit(f); }
+void Interpret::visit(llvm::BasicBlock &b) { Base::visit(b); }
+void Interpret::visit(llvm::Instruction &i) { llvm::errs() << i << "\n"; }
--- src/interpret.hpp
+++ src/interpret.hpp
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_GUARD_B3D1167F_2A1F_4D42_BE6F_DF2090D8F177
 #define INCLUDE_GUARD_B3D1167F_2A1F_4D42_BE6F_DF2090D8F177
 
+#include <llvm/IR/InstVisitor.h>
+
+class Interpret : public llvm::InstVisitor<Interpret> {
+  using Base = llvm::InstVisitor<Interpret>;
+
+ public:
+  void visit(llvm::Function &f);
+  void visit(llvm::BasicBlock &b);
+  void visit(llvm::Instruction &i);
+};
+
 #endif  // INCLUDE_GUARD_B3D1167F_2A1F_4D42_BE6F_DF2090D8F177
