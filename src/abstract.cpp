11f44cafd15be859ef931c50fb6f4238934a0e2c
hatsusato <knightspm5@gmail.com>
2019-08-26 14:54:48 +0900

    Add Abstraction

--- src/abstract.cpp
+++ src/abstract.cpp
@@ -1,1 +1,7 @@
 #include "abstract.hpp"
+#include "interpret.hpp"
+
+void Abstraction::interpret(llvm::Function &f) {
+  Interpret interpreter;
+  interpreter.visit(f);
+}
--- src/abstract.hpp
+++ src/abstract.hpp
@@ -1,4 +1,13 @@
 #ifndef INCLUDE_GUARD_FCDC6E4A_7148_4D58_920E_D9467F2A6CBA
 #define INCLUDE_GUARD_FCDC6E4A_7148_4D58_920E_D9467F2A6CBA
 
+namespace llvm {
+class Function;
+}
+
+class Abstraction {
+ public:
+  void interpret(llvm::Function &f);
+};
+
 #endif  // INCLUDE_GUARD_FCDC6E4A_7148_4D58_920E_D9467F2A6CBA
