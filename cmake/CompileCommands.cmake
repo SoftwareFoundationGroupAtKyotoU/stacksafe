5c30d678fa036061721b7d48a8974450e860581d
hatsusato <knightspm5@gmail.com>
2019-08-26 18:57:26 +0900

    Use override_cache

--- cmake/CompileCommands.cmake
+++ cmake/CompileCommands.cmake
@@ -3,10 +3,9 @@ function(export_compile_commands)
   set(source_file "${CMAKE_BINARY_DIR}/${json_file}")
   set(target_file "${CMAKE_SOURCE_DIR}/${json_file}")
 
+  include(OverrideCache)
   if("${CMAKE_BUILD_TYPE}" STREQUAL Debug)
-    get_property(docstring CACHE CMAKE_EXPORT_COMPILE_COMMANDS PROPERTY HELPSTRING)
-    set(CMAKE_EXPORT_COMPILE_COMMANDS ON
-      CACHE BOOL "${docstring}" FORCE)
+    override_cache(CMAKE_EXPORT_COMPILE_COMMANDS ON)
   endif()
   if(EXISTS "${source_file}")
     configure_file("${source_file}" "${target_file}" COPYONLY)
