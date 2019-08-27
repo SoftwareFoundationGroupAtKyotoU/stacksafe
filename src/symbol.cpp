7e7b9f11a363db564aafa9e3d24ba023c65292e6
hatsusato <knightspm5@gmail.com>
2019-08-27 19:00:18 +0900

    Fix to_json for Symbol

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -1,4 +1,5 @@
 #include "symbol.hpp"
+#include "io.hpp"
 
 namespace stacksafe {
 
@@ -15,6 +16,11 @@ std::string Symbol::to_str() const {
 bool operator<(const Symbol &lhs, const Symbol &rhs) {
   return lhs.get_num() < rhs.get_num();
 }
-void to_json(Json &j, const Symbol &x) { j = x.to_str(); }
+void to_json(Json &j, const Symbol &x) {
+  std::string str{"#"};
+  str += std::to_string(x.get_num());
+  str += type_tag(x.get_type());
+  j = str;
+}
 
 }  // namespace stacksafe
