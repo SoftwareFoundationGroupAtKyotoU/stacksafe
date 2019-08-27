037f2df45d5c148c3a82195fc7f7c14f8fe09c7a
hatsusato <knightspm5@gmail.com>
2019-08-27 16:31:44 +0900

    Add Domain

--- src/set.hpp
+++ src/set.hpp
@@ -2,6 +2,7 @@
 #define INCLUDE_GUARD_B43C0EC1_4338_47A3_AD6C_40B0DEF734B0
 
 #include <set>
+#include "symbol.hpp"
 
 namespace stacksafe {
 
@@ -9,6 +10,8 @@ template <typename T>
 class Set : private std::set<T> {
   using Base = std::set<T>;
 };
+
+class Domain : private std::set<Symbol> {};
 
 }  // namespace stacksafe
 
