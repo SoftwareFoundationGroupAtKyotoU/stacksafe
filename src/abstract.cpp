e01cd63190ce8451f5092af48a36f75545a643a2
hatsusato <knightspm5@gmail.com>
2019-08-27 14:01:20 +0900

    Add Abstraction::show

--- src/abstract.cpp
+++ src/abstract.cpp
@@ -1,5 +1,6 @@
 #include "abstract.hpp"
 #include "interpret.hpp"
+#include "nlohmann/json.hpp"
 
 void Abstraction::interpret(llvm::Function& f) {
   Interpret interpreter(*this);
@@ -7,4 +8,12 @@ void Abstraction::interpret(llvm::Function& f) {
 }
 void Abstraction::alloc(const Register& r) {
   env_.try_emplace(r, Symbol::create());
+}
+void Abstraction::show() const {
+  using json = nlohmann::json;
+  json j;
+  for (auto& [k, v] : env_) {
+    j[k.to_str()] = v.to_str();
+  }
+  llvm::errs() << j.dump(2) << "\n";
 }
--- src/abstract.hpp
+++ src/abstract.hpp
@@ -17,6 +17,7 @@ class Abstraction {
  public:
   void interpret(llvm::Function& f);
   void alloc(const Register& r);
+  void show() const;
 };
 
 #endif  // INCLUDE_GUARD_FCDC6E4A_7148_4D58_920E_D9467F2A6CBA
