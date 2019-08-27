0db511f33e03891a5f31915b5212843427ea2410
hatsusato <knightspm5@gmail.com>
2019-08-27 18:34:37 +0900

    Add string_from_stream

--- src/io.cpp
+++ src/io.cpp
@@ -1,6 +1,16 @@
 #include "io.hpp"
+#include <llvm/Support/raw_ostream.h>
 
 namespace stacksafe {
+namespace {
+template <typename F>
+std::string string_from_stream(F f) {
+  std::string buf;
+  llvm::raw_string_ostream stream{buf};
+  f(stream);
+  return stream.str();
+}
+}  // namespace
 
 std::optional<int> to_int(std::string_view view) {
   if (!view.empty()) {
