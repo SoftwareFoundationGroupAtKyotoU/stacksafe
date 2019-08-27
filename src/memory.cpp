a4d6c98da9051047f0fa4c627af57d6e0a6969b4
hatsusato <knightspm5@gmail.com>
2019-08-27 16:30:43 +0900

    Add env_to_json

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,21 +1,19 @@
 #include "memory.hpp"
 
 namespace stacksafe {
-
-void to_json(Json& j, const Stack& x) {
+namespace {
+template <typename Env>
+void env_to_json(Json& j, const Env& x) {
   Json tmp;
   for (auto& [k, v] : x) {
     tmp[k.to_str()] = v;
   }
   j = tmp;
 }
+}  // namespace
 
-void to_json(Json& j, const Heap& x) {
-  Json tmp;
-  for (auto& [k, v] : x) {
-    tmp[k.to_str()] = v;
-  }
-  j = tmp;
-}
+void to_json(Json& j, const Stack& x) { env_to_json(j, x); }
+
+void to_json(Json& j, const Heap& x) { env_to_json(j, x); }
 
 }  // namespace stacksafe
