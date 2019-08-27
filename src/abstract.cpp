e6ed8fc6856a02d6c58ca8ae3dafbee277c853d4
hatsusato <knightspm5@gmail.com>
2019-08-27 14:07:35 +0900

    Impl to_json for stack and heap

--- src/abstract.cpp
+++ src/abstract.cpp
@@ -2,6 +2,23 @@
 #include "interpret.hpp"
 #include "nlohmann/json.hpp"
 
+namespace {
+nlohmann::json to_json(const std::map<Register, Symbol>& stack) {
+  nlohmann::json j;
+  for (auto& [k, v] : stack) {
+    j[k.to_str()] = v.to_str();
+  }
+  return j;
+}
+nlohmann::json to_json(const std::map<Symbol, Symbol>& heap) {
+  nlohmann::json j;
+  for (auto& [k, v] : heap) {
+    j[k.to_str()] = v.to_str();
+  }
+  return j;
+}
+}  // namespace
+
 void Abstraction::interpret(llvm::Function& f) {
   Interpret interpreter(*this);
   interpreter.visit(f);
