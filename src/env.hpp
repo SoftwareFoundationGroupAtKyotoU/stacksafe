7be40866cbf36fc73d2211a4ccbb1651b368a1b8
hatsusato <knightspm5@gmail.com>
2019-08-27 16:19:30 +0900

    Add Env

--- src/env.hpp
+++ src/env.hpp
@@ -1,4 +1,21 @@
 #ifndef INCLUDE_GUARD_A0BA2711_AA71_4105_83AF_E6AF119E4855
 #define INCLUDE_GUARD_A0BA2711_AA71_4105_83AF_E6AF119E4855
 
+#include <map>
+#include <set>
+#include "symbol.hpp"
+
+namespace stacksafe {
+
+template <typename Key>
+class Env : private std::map<Key, std::set<Symbol>> {
+  using Val = std::set<Symbol>;
+  using Base = std::map<Key, Val>;
+
+ public:
+  using Base::begin, Base::end;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_A0BA2711_AA71_4105_83AF_E6AF119E4855
