6c8f652a84919775671bf5f460c5ef7c4026d0b6
hatsusato <knightspm5@gmail.com>
2019-08-27 15:07:06 +0900

    Add alias for json

--- src/json.hpp
+++ src/json.hpp
@@ -1,4 +1,12 @@
 #ifndef INCLUDE_GUARD_E372F549_B09F_460E_B778_3AFBC559D1E8
 #define INCLUDE_GUARD_E372F549_B09F_460E_B778_3AFBC559D1E8
 
+#include <nlohmann/json.hpp>
+
+namespace stacksafe {
+
+using Json = nlohmann::json;
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_E372F549_B09F_460E_B778_3AFBC559D1E8
