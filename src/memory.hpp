1dbf16bc85fa38039c61ea68f43ff9ada8ae114e
hatsusato <knightspm5@gmail.com>
2019-08-27 15:13:23 +0900

    Add Stack

--- src/memory.hpp
+++ src/memory.hpp
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_GUARD_131C5103_4229_44F2_8D1D_95A9AC6AC944
 #define INCLUDE_GUARD_131C5103_4229_44F2_8D1D_95A9AC6AC944
 
+#include <map>
+#include <set>
+#include "register.hpp"
+#include "symbol.hpp"
+
+namespace stacksafe {
+
+class Stack : private std::map<Register, std::set<Symbol>> {};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_131C5103_4229_44F2_8D1D_95A9AC6AC944
