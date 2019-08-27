7810f1af0e9728975ee42200501ed1533fa46ec4
hatsusato <knightspm5@gmail.com>
2019-08-27 18:55:24 +0900

    Add Symbol::get_num

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -5,6 +5,7 @@ namespace stacksafe {
 int Symbol::current_ = 0;
 Symbol::Symbol(int n, const llvm::Type *t) : num_{n}, type_{t} {}
 Symbol Symbol::create(const llvm::Type *t) { return Symbol{current_++, t}; }
+int Symbol::get_num() const { return num_; }
 bool Symbol::compare(const Symbol &that) const { return num_ < that.num_; }
 std::string Symbol::to_str() const {
   using namespace std::string_literals;
