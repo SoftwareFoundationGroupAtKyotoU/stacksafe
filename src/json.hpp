f042f4f52ff1d694913b3f2d8e86dd1193b1d41d
hatsusato <knightspm5@gmail.com>
2019-08-29 16:09:33 +0900

    Add json_fwd

--- src/json.hpp
+++ src/json.hpp
@@ -2,11 +2,8 @@
 #define INCLUDE_GUARD_E372F549_B09F_460E_B778_3AFBC559D1E8
 
 #include <nlohmann/json.hpp>
+#include "json_fwd.hpp"
 
-namespace stacksafe {
-
-using Json = nlohmann::json;
-
-}  // namespace stacksafe
+namespace stacksafe {}  // namespace stacksafe
 
 #endif  // INCLUDE_GUARD_E372F549_B09F_460E_B778_3AFBC559D1E8
---
+++ src/json_fwd.hpp
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_GUARD_50056085_41BD_471A_9C7A_BD1B40E216C7
+#define INCLUDE_GUARD_50056085_41BD_471A_9C7A_BD1B40E216C7
+
+#include <nlohmann/json_fwd.hpp>
+
+namespace stacksafe {
+
+using Json = nlohmann::json;
+
+}
+
+#endif  // INCLUDE_GUARD_50056085_41BD_471A_9C7A_BD1B40E216C7
