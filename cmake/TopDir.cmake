baa3c0bdf7c92d6c943f87873b9c31641afa3fc2
hatsusato <knightspm5@gmail.com>
2019-08-30 12:13:01 +0900

    Fix TopDir

--- cmake/TopDir.cmake
+++ cmake/TopDir.cmake
@@ -1,3 +1,3 @@
-function(set_top_dir)
-  set(TOP_SOURCE_DIR "${${PROJECT_NAME}_SOURCE_DIR}" PARENT_SCOPE)
-endfunction()
+if(NOT DEFINED TOP_SOURCE_DIR)
+  set(TOP_SOURCE_DIR "${${PROJECT_NAME}_SOURCE_DIR}" )
+endif()
