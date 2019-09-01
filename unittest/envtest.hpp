08475e8e7772bb7076c313d8fd3072e63835e55f
hatsusato <knightspm5@gmail.com>
2019-09-01 18:10:13 +0900

    Add EnvTest

--- unittest/envtest.hpp
+++ unittest/envtest.hpp
@@ -1,4 +1,13 @@
 #ifndef INCLUDE_GUARD_C7E72859_0086_4AA2_8E53_FAAE8ED32D45
 #define INCLUDE_GUARD_C7E72859_0086_4AA2_8E53_FAAE8ED32D45
 
+#include <gtest/gtest.h>
+#include "env.hpp"
+#include "json.hpp"
+
+struct EnvTest : ::testing::Test {
+  stacksafe::Env env;
+  stacksafe::Json expect;
+};
+
 #endif  // INCLUDE_GUARD_C7E72859_0086_4AA2_8E53_FAAE8ED32D45
