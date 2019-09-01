5af8dc5e974864ce3faf63a01646b344b6ae0102
hatsusato <knightspm5@gmail.com>
2019-09-01 17:40:18 +0900

    Support develop target for check.sh

--- check.sh
+++ check.sh
@@ -7,9 +7,14 @@ if [[ "$1" == -i ]]; then
     exit
 fi
 cmake .. -GNinja || exit $?
-ninja || exit $?
-GTEST_COLOR=1 ctest -V
-popd
-if [[ -n "$1" ]]; then
+if [[ "$1" == develop ]]; then
+    ninja develop
+    exit
+elif [[ -z "$1" ]]; then
+    ninja develop || exit $?
+    GTEST_COLOR=1 ctest -V
+else
+    ninja || exit $?
+    popd
     opt -analyze -load=build/stacksafe.so -stacksafe "$1"
 fi
