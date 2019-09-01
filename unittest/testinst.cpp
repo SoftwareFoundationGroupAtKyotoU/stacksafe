26323045263315c5565f0a54cb320423a14aa484
hatsusato <knightspm5@gmail.com>
2019-09-01 18:49:21 +0900

    Add EnvTest::Load

--- unittest/testinst.cpp
+++ unittest/testinst.cpp
@@ -28,3 +28,22 @@ TEST_F(EnvTest, Store) {
   push_heap(1, 0);
   equal();
 }
+
+TEST_F(EnvTest, Load) {
+  V v0{0}, v1{1}, v2{2};
+  equal();
+  env.alloc(v0);
+  init_heap(0);
+  push_stack(0, 0);
+  equal();
+  env.alloc(v1);
+  init_heap(1);
+  push_stack(1, 1);
+  equal();
+  env.store(v0, v1);
+  push_heap(1, 0);
+  equal();
+  env.load(v2, v1);
+  push_stack(2, 0);
+  equal();
+}
