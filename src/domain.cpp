b4024c4d5482652d48d1ec3173723f7a4bdf90e9
hatsusato <knightspm5@gmail.com>
2019-09-02 15:09:09 +0900

    Add Domain::includes

--- src/domain.cpp
+++ src/domain.cpp
@@ -5,6 +5,9 @@ namespace stacksafe {
 
 void Domain::insert(const Symbol& sym) { Base::insert(sym); }
 void Domain::insert(const Domain& dom) { Base::insert(dom.begin(), dom.end()); }
+bool Domain::includes(const Domain& that) const {
+  return std::includes(begin(), end(), that.begin(), that.end());
+}
 void to_json(Json& j, const Domain& x) {
   Json tmp;
   for (auto& e : x) {
