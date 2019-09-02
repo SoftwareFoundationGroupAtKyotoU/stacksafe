e7ef7369e0f2e7e182130de669aa358452dc70de
hatsusato <knightspm5@gmail.com>
2019-09-02 17:45:40 +0900

    Add Todo

--- src/abstract.hpp
+++ src/abstract.hpp
@@ -2,18 +2,25 @@
 #define INCLUDE_GUARD_0E783D0B_1A5B_4270_A79B_0594D4E3D6CB
 
 #include <map>
+#include <set>
 #include "env.hpp"
 #include "json_fwd.hpp"
 
 namespace llvm {
 class BasicBlock;
 class Function;
+class Instruction;
 }  // namespace llvm
 
 namespace stacksafe {
 
+struct Todo : private std::set<llvm::Instruction*> {
+  using Base = std::set<llvm::Instruction*>;
+};
+
 class Abstract {
   std::map<llvm::BasicBlock*, Env> blocks_;
+  Todo todo_;
   friend void to_json(Json& j, const Abstract& x);
 
  public:
