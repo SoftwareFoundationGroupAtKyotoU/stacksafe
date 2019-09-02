f2ae40a343932da23df85a536b3a1612e8f739a7
hatsusato <knightspm5@gmail.com>
2019-09-02 14:03:13 +0900

    Add Abstract

--- src/abstract.hpp
+++ src/abstract.hpp
@@ -1,4 +1,19 @@
 #ifndef INCLUDE_GUARD_0E783D0B_1A5B_4270_A79B_0594D4E3D6CB
 #define INCLUDE_GUARD_0E783D0B_1A5B_4270_A79B_0594D4E3D6CB
 
+#include <map>
+#include "env.hpp"
+
+namespace llvm {
+class BasicBlock;
+}
+
+namespace stacksafe {
+
+class Abstract {
+  std::map<llvm::BasicBlock*, Env> blocks_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_0E783D0B_1A5B_4270_A79B_0594D4E3D6CB
