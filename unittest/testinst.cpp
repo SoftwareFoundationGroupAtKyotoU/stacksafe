b5912ded0e2e439525307587f9eb1a640f599952
hatsusato <knightspm5@gmail.com>
2019-09-05 14:35:47 +0900

    Add EnvTest::Call

--- unittest/testinst.cpp
+++ unittest/testinst.cpp
@@ -55,3 +55,36 @@ TEST_F(EnvTest, Constant) {
   init_stack(0);
   equal();
 }
+
+TEST_F(EnvTest, Call) {
+  V v0{0}, v1{1}, v2{2}, v3{3};
+  equal();
+  env.alloc(v0);
+  init_heap(0);
+  push_stack(0, 0);
+  equal();
+  env.alloc(v1);
+  init_heap(1);
+  push_stack(1, 1);
+  equal();
+  env.alloc(v2);
+  init_heap(2);
+  push_stack(2, 2);
+  equal();
+  env.store(v1, v0);
+  push_heap(0, 1);
+  equal();
+  env.store(v2, v0);
+  push_heap(0, 2);
+  equal();
+  Params params{v0, v1};
+  env.call(v3, params);
+  auto &key0 = expect["heap"][sym(0)];
+  key0.insert(key0.begin(), sym(0));
+  for (int i = 0; i < 3; ++i) {
+    push_heap(1, i);
+    push_heap(2, i);
+    push_stack(3, i);
+  }
+  equal();
+}
