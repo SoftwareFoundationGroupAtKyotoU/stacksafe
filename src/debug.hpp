bb3fbe1c52c07bcd01c25361b242d0178f54077c
hatsusato <knightspm5@gmail.com>
2019-09-06 15:48:16 +0900

    Add LogBlock

--- src/debug.hpp
+++ src/debug.hpp
@@ -1,4 +1,19 @@
 #ifndef INCLUDE_GUARD_91A166AC_CDE2_42E8_B393_EEBD12227F6A
 #define INCLUDE_GUARD_91A166AC_CDE2_42E8_B393_EEBD12227F6A
 
+#include "env.hpp"
+
+namespace llvm {
+class BasicBlock;
+}
+
+namespace stacksafe {
+
+struct LogBlock {
+  const Env before, after;
+  llvm::BasicBlock* block;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_91A166AC_CDE2_42E8_B393_EEBD12227F6A
