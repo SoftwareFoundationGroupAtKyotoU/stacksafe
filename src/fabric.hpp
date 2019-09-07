6dfe7d03ce5726a0ce1af1dbe625b272258553df
hatsusato <knightspm5@gmail.com>
2019-09-07 14:02:23 +0900

    Add Fabric

--- src/fabric.hpp
+++ src/fabric.hpp
@@ -1,4 +1,16 @@
 #ifndef INCLUDE_GUARD_D970F1B9_9CD5_4979_89FF_67935A12C1D8
 #define INCLUDE_GUARD_D970F1B9_9CD5_4979_89FF_67935A12C1D8
 
+#include <string>
+#include <vector>
+
+namespace stacksafe {
+
+class Fabric {
+  std::vector<std::string> fabric_;
+  std::size_t pos_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_D970F1B9_9CD5_4979_89FF_67935A12C1D8
