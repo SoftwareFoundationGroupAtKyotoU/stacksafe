2402272dab07fa44bc25e259a1e4d359fe8794cf
hatsusato <knightspm5@gmail.com>
2019-09-07 16:25:41 +0900

    Fix {LogBlock,Log}::print

--- src/debug.cpp
+++ src/debug.cpp
@@ -11,14 +11,21 @@ namespace stacksafe {
 LogBlock::LogBlock(const Env& p, const llvm::BasicBlock* b, const Env& n)
     : prev{p}, next{n}, block{b} {}
 void LogBlock::print() const {
-  const auto hr = "----------------", endl = "\n";
-  Fabric left, right;
-  left.append("[prev]").next().append(dump(prev));
-  right.append("[next]").next().append(dump(next));
-  llvm::errs() << hr << endl;
-  llvm::errs() << *block << endl;
-  llvm::errs() << left.patch(right.indent(2)) << endl;
-  llvm::errs() << hr << endl;
+  Fabric env;
+  {
+    Fabric left, right;
+    left.append("heap: [prev]").next().append(dump(prev.heap()));
+    right.append("heap: [next]").next().append(dump(next.heap()));
+    env.append(left.patch(right.indent(2))).next();
+  }
+  {
+    Fabric left, right;
+    left.append("stack: [prev]").next().append(dump(prev.stack()));
+    right.append("stack: [next]").next().append(dump(next.stack()));
+    env.append(left.patch(right.indent(2))).next();
+  }
+  llvm::errs() << *block;
+  llvm::errs() << env;
 }
 
 Log::Log(const llvm::Function& func) : function{&func} {}
@@ -26,9 +33,11 @@ void Log::add(const Env& prev, const llvm::BasicBlock* block, const Env& next) {
   blocks.emplace_back(prev, block, next);
 }
 void Log::print() const {
+  const auto hr = "--------------------------------";
   llvm::errs() << *function;
   for (auto& log : blocks) {
     log.print();
+    llvm::errs() << "\n" << hr << "\n";
   }
 }
 
