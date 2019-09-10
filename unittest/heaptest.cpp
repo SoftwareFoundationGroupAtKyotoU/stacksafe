2e085a17b65ca431f64cef42e8180276fd986416
hatsusato <knightspm5@gmail.com>
2019-09-10 12:36:33 +0900

    Add HeapTest::Initial test

--- unittest/heaptest.cpp
+++ unittest/heaptest.cpp
@@ -1,5 +1,7 @@
 #include "heaptest.hpp"
 
+TEST_F(HeapTest, Initial) { equal(); }
+
 void HeapTest::SetUp() { json = stacksafe::Json::object(); }
 void HeapTest::equal() const {
   stacksafe::Json tmp = heap;
