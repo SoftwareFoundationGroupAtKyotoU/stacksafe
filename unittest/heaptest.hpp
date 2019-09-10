1913b4a0f5853ff64b5e3f02cf9a0aef73574060
hatsusato <knightspm5@gmail.com>
2019-09-10 12:32:05 +0900

    Add HeapTest

--- unittest/heaptest.hpp
+++ unittest/heaptest.hpp
@@ -1,4 +1,13 @@
 #ifndef INCLUDE_GUARD_35398468_D0B7_4EC4_86F5_6DFBAD5C5824
 #define INCLUDE_GUARD_35398468_D0B7_4EC4_86F5_6DFBAD5C5824
 
+#include <gtest/gtest.h>
+#include "json.hpp"
+#include "map.hpp"
+
+struct HeapTest : ::testing::Test {
+  stacksafe::Heap heap;
+  stacksafe::Json json;
+};
+
 #endif  // INCLUDE_GUARD_35398468_D0B7_4EC4_86F5_6DFBAD5C5824
