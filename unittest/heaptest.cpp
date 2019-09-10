8bf015df467e590e05f6b01dd94b543fcee6936a
hatsusato <knightspm5@gmail.com>
2019-09-10 12:53:05 +0900

    Add HeapTest::InsertDomain test

--- unittest/heaptest.cpp
+++ unittest/heaptest.cpp
@@ -15,6 +15,25 @@ TEST_F(HeapTest, InsertSymbol) {
   push(b, c);
   equal();
 }
+TEST_F(HeapTest, InsertDomain) {
+  stacksafe::Domain dom0, dom1;
+  auto s0 = symbol(), s1 = symbol();
+  auto a = "a", b = "b", c = "c", d = "d";
+  dom0.insert(symbol());
+  dom0.insert(symbol());
+  dom1.insert(dom0);
+  dom0.insert(s1);
+  equal();
+  heap.insert(s0, dom0);
+  push(a, b);
+  push(a, c);
+  push(a, d);
+  equal();
+  heap.insert(s1, dom1);
+  push(b, c);
+  push(b, d);
+  equal();
+}
 
 void HeapTest::SetUp() { json = stacksafe::Json::object(); }
 void HeapTest::push(std::string key, std::string val) {
