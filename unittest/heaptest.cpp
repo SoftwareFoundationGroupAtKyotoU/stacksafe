fe38cc7aa297f58158548c69f61ee7b29b3253c5
hatsusato <knightspm5@gmail.com>
2019-09-10 12:36:26 +0900

    Add HeapTest::SetUp

--- unittest/heaptest.cpp
+++ unittest/heaptest.cpp
@@ -1,5 +1,6 @@
 #include "heaptest.hpp"
 
+void HeapTest::SetUp() { json = stacksafe::Json::object(); }
 void HeapTest::equal() const {
   stacksafe::Json tmp = heap;
   EXPECT_EQ(json.dump(), tmp.dump());
--- unittest/heaptest.hpp
+++ unittest/heaptest.hpp
@@ -6,6 +6,7 @@
 #include "map.hpp"
 
 struct HeapTest : ::testing::Test {
+  void SetUp();
   void equal() const;
   stacksafe::Heap heap;
   stacksafe::Json json;
