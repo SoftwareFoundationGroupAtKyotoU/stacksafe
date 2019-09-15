eb0f66f29ada6f22ccf0aba57c31c1f88be6201f
hatsusato <knightspm5@gmail.com>
2019-09-15 16:06:49 +0900

    Fix Type::repr and to_json for Type

--- src/type.cpp
+++ src/type.cpp
@@ -10,7 +10,7 @@ Type::Type(const llvm::Type* t) : type_{t} {}
 const llvm::Type* Type::get() const { return type_; }
 std::string Type::repr() const {
   if (type_) {
-    return to_str(*type_);
+    return "<" + to_str(*type_) + ">";
   }
   return {};
 }
@@ -23,7 +23,7 @@ bool Type::is_pointer() const {
 Type Type::pointee_type() const {
   return Type{is_pointer() ? type_->getPointerElementType() : nullptr};
 }
-void to_json(Json& j, const Type& x) { j = "<" + x.repr() + ">"; }
+void to_json(Json& j, const Type& x) { j = x.repr(); }
 Fabric dump(const Type& type) {
   Fabric ret;
   if (type.get()) {
