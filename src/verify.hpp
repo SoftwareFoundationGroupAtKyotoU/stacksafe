ff0bc7eca5f981ccafc44264279dd5073873aefc
hatsusato <knightspm5@gmail.com>
2019-09-19 15:56:02 +0900

    Add Verifier

--- src/verify.hpp
+++ src/verify.hpp
@@ -1,4 +1,12 @@
 #ifndef INCLUDE_GUARD_8CFD1EA5_B0CA_49A1_AE42_BA8D4F1F41DF
 #define INCLUDE_GUARD_8CFD1EA5_B0CA_49A1_AE42_BA8D4F1F41DF
 
+#include <llvm/IR/InstVisitor.h>
+
+namespace stacksafe {
+
+class Verifier : public llvm::InstVisitor<Verifier, void> {};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_8CFD1EA5_B0CA_49A1_AE42_BA8D4F1F41DF
