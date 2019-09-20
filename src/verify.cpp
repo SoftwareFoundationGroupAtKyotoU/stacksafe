be41b27593a1acfa0dbba9bc72faca62da70d4d0
hatsusato <knightspm5@gmail.com>
2019-09-20 11:20:27 +0900

    Fix Verifier::visitCallInst

--- src/verify.cpp
+++ src/verify.cpp
@@ -24,13 +24,9 @@ auto Verifier::visit(const llvm::BasicBlock &b) -> RetTy {
 auto Verifier::visitCallInst(llvm::CallInst &i) -> RetTy {
   for (auto &use : i.args()) {
     if (auto arg = use.get()) {
-      auto reg = Value::make(*arg);
-      if (auto dom = env_.stack().get(reg)) {
-        if (dom->has_local() && dom->includes(Symbol::global())) {
-          return unsafe;
-        }
-      } else {
-        stacksafe_unreachable("unknown register", reg);
+      auto dom = env_.from_stack(Value::make(*arg));
+      if (dom.has_local() && dom.includes(Symbol::global())) {
+        return unsafe;
       }
     } else {
       stacksafe_unreachable("unknown parameter", i);
