e148e7e8454532fc2acb9599a161f28634cd517d
hatsusato <knightspm5@gmail.com>
2019-09-27 10:30:15 +0900

    Add Env::Env

--- src/env.cpp
+++ src/env.cpp
@@ -1,1 +1,7 @@
 #include "env.hpp"
+
+namespace stacksafe {
+
+Env::Env(Memory& m, RegisterCache& r) : mem_{m}, cache_{r} {}
+
+}  // namespace stacksafe
--- src/env.hpp
+++ src/env.hpp
@@ -2,8 +2,16 @@
 #define INCLUDE_GUARD_C088E002_5D7C_41FC_929E_102BD43FCAEA
 
 namespace stacksafe {
+class Memory;
+class RegisterCache;
 
-class Env {};
+class Env {
+  Memory& mem_;
+  RegisterCache& cache_;
+
+ public:
+  Env(Memory& m, RegisterCache& r);
+};
 
 }  // namespace stacksafe
 
