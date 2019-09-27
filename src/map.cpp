d89013c76721b780f56f63f6ad7ace9298b74ddc
hatsusato <knightspm5@gmail.com>
2019-09-27 16:45:19 +0900

    Remove instantiation of Map<Value>

--- src/map.cpp
+++ src/map.cpp
@@ -81,15 +81,12 @@ Fabric dump(const Map<K>& map) {
   return ret.append("}");
 }
 
-template class Map<Value>;
 template class Map<Symbol>;
 template class Map<Register>;
 
-template void to_json<Value>(Json&, const Stack&);
 template void to_json<Symbol>(Json&, const Heap&);
 template void to_json<Register>(Json&, const Map<Register>&);
 
-template Fabric dump<Value>(const Stack&);
 template Fabric dump<Symbol>(const Heap&);
 template Fabric dump<Register>(const Map<Register>&);
 
--- src/map.hpp
+++ src/map.hpp
@@ -30,19 +30,15 @@ void to_json(Json& j, const Map<K>& x);
 template <typename K>
 Fabric dump(const Map<K>& map);
 
-using Stack = Map<Value>;
 using Heap = Map<Symbol>;
 using RegMap = Map<Register>;
 
-extern template class Map<Value>;
 extern template class Map<Symbol>;
 extern template class Map<Register>;
 
-extern template void to_json<Value>(Json&, const Stack&);
 extern template void to_json<Symbol>(Json&, const Heap&);
 extern template void to_json<Register>(Json&, const Map<Register>&);
 
-extern template Fabric dump<Value>(const Stack&);
 extern template Fabric dump<Symbol>(const Heap&);
 extern template Fabric dump<Register>(const Map<Register>&);
 
