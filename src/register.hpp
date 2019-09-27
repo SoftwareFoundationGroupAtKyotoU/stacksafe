acefc700956d28cc99e9a518855b25d780bd9f92
hatsusato <knightspm5@gmail.com>
2019-09-27 09:57:00 +0900

    Add Register

--- src/register.hpp
+++ src/register.hpp
@@ -1,4 +1,12 @@
 #ifndef INCLUDE_GUARD_339D16F3_498E_420D_A302_BE9C21A36707
 #define INCLUDE_GUARD_339D16F3_498E_420D_A302_BE9C21A36707
 
+namespace stacksafe {
+
+class Register {
+  int num_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_339D16F3_498E_420D_A302_BE9C21A36707
