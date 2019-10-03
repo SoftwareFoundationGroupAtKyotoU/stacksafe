e9aee3bdbfbf7b7c7db38466186516b570d3d8d0
hatsusato <knightspm5@gmail.com>
2019-10-03 15:18:46 +0900

    Fix Env::Env

--- src/env.cpp
+++ src/env.cpp
@@ -11,6 +11,7 @@ void Params::insert(const llvm::Value &v) {
 Env::Env(const Cache &c, const Memory &m) : cache_{c}, mem_{m} {}
 Env::Env(Cache &c, const llvm::Function &f) : cache_{c} {
   auto g = Domain::global();
+  insert(Symbol::global(), g);
   for (const auto &a : f.args()) {
     c.add(a);
     insert(a, g);
