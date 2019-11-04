cfa541ac048371fed147077459ad797c718dd756
hatsusato <knightspm5@gmail.com>
2019-11-04 10:37:30 +0900

    Add Number

--- src/number.cpp
+++ src/number.cpp
@@ -1,1 +1,7 @@
 #include "number.hpp"
+
+namespace stacksafe {
+
+Number::Number(int n) : num_{n} {}
+
+}  // namespace stacksafe
--- src/number.hpp
+++ src/number.hpp
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_GUARD_C1AC39A1_F679_4ECB_9D0B_D02FAC20B878
 #define INCLUDE_GUARD_C1AC39A1_F679_4ECB_9D0B_D02FAC20B878
 
+namespace stacksafe {
+
+class Number {
+  const int num_;
+
+ public:
+  explicit Number(int n);
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_C1AC39A1_F679_4ECB_9D0B_D02FAC20B878
