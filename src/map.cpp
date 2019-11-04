b112dcee47cc12764cc2db7ca8d4c8360247cec6
hatsusato <knightspm5@gmail.com>
2019-11-04 17:49:59 +0900

    Remove Heap and Stack

--- src/map.cpp
+++ src/map.cpp
@@ -39,44 +39,4 @@ void to_json(Json &j, const Map &x) {
   j = obj;
 }
 
-void Heap::insert(const Register &key, const Domain &val) {
-  Super::insert(key, val);
-}
-const Domain &Heap::lookup(const Register &key) const {
-  return Super::lookup(key);
-}
-void Heap::merge(const Heap &that) {
-  Super::merge(that);
-}
-bool Heap::includes(const Heap &that) const {
-  return Super::includes(that);
-}
-void to_json(Json &j, const Heap &x) {
-  Json::object_t obj;
-  for (const auto &[key, val] : x) {
-    obj[to_str(key)] = val;
-  }
-  j = obj;
-}
-
-void Stack::insert(const Register &key, const Domain &val) {
-  Super::insert(key, val);
-}
-const Domain &Stack::lookup(const Register &key) const {
-  return Super::lookup(key);
-}
-void Stack::merge(const Stack &that) {
-  Super::merge(that);
-}
-bool Stack::includes(const Stack &that) const {
-  return Super::includes(that);
-}
-void to_json(Json &j, const Stack &x) {
-  Json::object_t obj;
-  for (const auto &[key, val] : x) {
-    obj[to_str(key)] = val;
-  }
-  j = obj;
-}
-
 }  // namespace stacksafe
--- src/map.hpp
+++ src/map.hpp
@@ -22,30 +22,6 @@ class Map : private std::map<Register, Domain> {
 };
 void to_json(Json &j, const Map &x);
 
-class Heap : private Map {
-  using Super = Map;
-
- public:
-  using Super::begin, Super::end;
-  void insert(const Register &key, const Domain &val);
-  const Domain &lookup(const Register &key) const;
-  void merge(const Heap &that);
-  bool includes(const Heap &that) const;
-};
-void to_json(Json &j, const Heap &x);
-
-class Stack : private Map {
-  using Super = Map;
-
- public:
-  using Super::begin, Super::end;
-  void insert(const Register &key, const Domain &val);
-  const Domain &lookup(const Register &key) const;
-  void merge(const Stack &that);
-  bool includes(const Stack &that) const;
-};
-void to_json(Json &j, const Stack &x);
-
 }  // namespace stacksafe
 
 #endif  // INCLUDE_GUARD_A0BA2711_AA71_4105_83AF_E6AF119E4855
