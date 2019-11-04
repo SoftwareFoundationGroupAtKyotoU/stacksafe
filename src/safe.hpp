83d3d91a1cac8e87ba7cb2ceec8ed394cefb2dae
hatsusato <knightspm5@gmail.com>
2019-11-04 15:32:20 +0900

    Add Safe

--- src/safe.hpp
+++ src/safe.hpp
@@ -1,4 +1,12 @@
 #ifndef INCLUDE_GUARD_83A9DF1A_4262_4B6D_9E02_0C43F6C3474B
 #define INCLUDE_GUARD_83A9DF1A_4262_4B6D_9E02_0C43F6C3474B
 
+namespace stacksafe {
+
+class Safe {
+  bool safe_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_83A9DF1A_4262_4B6D_9E02_0C43F6C3474B
