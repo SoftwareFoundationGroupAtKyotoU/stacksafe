b79d26c29653ad8d3da00acf09232dca03ad25bd
hatsusato <knightspm5@gmail.com>
2019-11-04 10:39:58 +0900

    Inherit Register from Number

--- src/register.cpp
+++ src/register.cpp
@@ -2,10 +2,6 @@
 
 namespace stacksafe {
 
-Register::Register(int n) : num_{n} {}
-int Register::number() const {
-  return num_;
-}
 bool operator<(const Register &lhs, const Register &rhs) {
   return lhs.number() < rhs.number();
 }
--- src/register.hpp
+++ src/register.hpp
@@ -2,16 +2,16 @@
 #define INCLUDE_GUARD_339D16F3_498E_420D_A302_BE9C21A36707
 
 #include <string>
+#include "number.hpp"
 
 namespace stacksafe {
-class Fabric;
 
-class Register {
-  const int num_;
+class Register : private Number {
+  using Super = Number;
+  using Super::Super;
 
  public:
-  explicit Register(int n);
-  int number() const;
+  using Super::number;
 };
 bool operator<(const Register &lhs, const Register &rhs);
 std::string to_str(const Register &reg);
