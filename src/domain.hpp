543e60832d7d0901a038a66a6466c962c89d5656
hatsusato <knightspm5@gmail.com>
2019-11-04 16:01:42 +0900

    Add Domain::get_empty

--- src/domain.hpp
+++ src/domain.hpp
@@ -19,6 +19,7 @@ class Domain : private std::set<Symbol> {
   bool includes(const Domain &that) const;
   Domain minus(const Domain &that) const;
   bool has_local() const;
+  static const Domain &get_empty();
   static const Domain &get_global();
   static Domain global();
 };
