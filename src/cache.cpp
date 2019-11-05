20b040ea4db75b6809bf09e9c0111f86aa0d4448
hatsusato <knightspm5@gmail.com>
2019-11-05 13:24:41 +0900

    Update Cache::Cache

--- src/cache.cpp
+++ src/cache.cpp
@@ -4,7 +4,10 @@
 
 namespace stacksafe {
 
-Cache::Cache() : cache_{std::make_unique<Super>()} {}
+Cache::Cache() : cache_{std::make_unique<Super>()} {
+  assert(cache_ && "failed cache init");
+  cache_->try_emplace(nullptr, -1);
+}
 std::optional<int> Cache::lookup(const llvm::Value& reg) const {
   if (cache_) {
     if (auto it = cache_->find(&reg); it != cache_->end()) {
