7c7f128c3734b58c020667772574a9c845056dbd
hatsusato <knightspm5@gmail.com>
2019-11-05 19:24:58 +0900

    Add Value::hash

--- src/value.cpp
+++ src/value.cpp
@@ -1,10 +1,14 @@
 #include "value.hpp"
+#include <functional>
 
 namespace stacksafe {
 
 Value::Value(const llvm::Value* v) : val_{v} {}
 const llvm::Value* Value::value() const {
   return val_;
+}
+std::size_t Value::hash() const {
+  return std::hash<const llvm::Value*>{}(val_);
 }
 
 }  // namespace stacksafe
--- src/value.hpp
+++ src/value.hpp
@@ -1,6 +1,8 @@
 #ifndef INCLUDE_GUARD_649A01D6_F0F8_4589_8C81_4B1FCA2FD120
 #define INCLUDE_GUARD_649A01D6_F0F8_4589_8C81_4B1FCA2FD120
 
+#include <cstddef>
+
 namespace llvm {
 class Value;
 }
@@ -13,6 +15,7 @@ class Value {
  public:
   explicit Value(const llvm::Value* v);
   const llvm::Value* value() const;
+  std::size_t hash() const;
 };
 
 }  // namespace stacksafe
