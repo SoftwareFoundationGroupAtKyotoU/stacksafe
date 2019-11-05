aa7a8cd2071c462b43cfe02ff3227bcf16253432
hatsusato <knightspm5@gmail.com>
2019-11-05 19:14:07 +0900

    Add Register

--- src/register.hpp
+++ src/register.hpp
@@ -1,4 +1,16 @@
 #ifndef INCLUDE_GUARD_8F170DBC_839A_403D_A59A_EA8DF99056AA
 #define INCLUDE_GUARD_8F170DBC_839A_403D_A59A_EA8DF99056AA
 
+namespace llvm {
+class Value;
+}
+
+namespace stacksafe {
+
+class Register {
+  const llvm::Value* const val_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_8F170DBC_839A_403D_A59A_EA8DF99056AA
