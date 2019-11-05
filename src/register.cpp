028557ad673248ecc6ca28148f02dabee2eeed71
hatsusato <knightspm5@gmail.com>
2019-11-05 20:18:33 +0900

    Add is_global

--- src/register.cpp
+++ src/register.cpp
@@ -1,6 +1,23 @@
 #include "register.hpp"
+#include <llvm/IR/Instructions.h>
 
 namespace stacksafe {
+namespace {
+bool is_global(const llvm::Constant* c) {
+  if (llvm::isa<llvm::GlobalValue>(c)) {
+    return true;
+  }
+  for (unsigned i = 0; i < c->getNumOperands(); ++i) {
+    assert(llvm::isa<llvm::Constant>(c->getOperand(i)) &&
+           "constant has non-constant");
+    if (auto v = llvm::dyn_cast<llvm::Constant>(c->getOperand(i));
+        is_global(v)) {
+      return true;
+    }
+  }
+  return false;
+}
+}  // namespace
 
 Register::Register(const llvm::Value& v) : Value{&v} {}
 
--- src/register.hpp
+++ src/register.hpp
@@ -7,7 +7,7 @@
 
 namespace llvm {
 class Value;
-}
+}  // namespace llvm
 
 namespace stacksafe {
 
