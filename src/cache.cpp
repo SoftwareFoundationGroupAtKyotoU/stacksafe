13263c41fd435609e4ac7e071e4364c729e63e5d
hatsusato <knightspm5@gmail.com>
2019-11-05 16:23:33 +0900

    Add to_int

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,8 +1,34 @@
 #include "cache.hpp"
+#include <optional>
+#include <string_view>
 #include "register.hpp"
 #include "utility.hpp"
 
 namespace stacksafe {
+namespace {
+std::optional<int> to_int(std::string_view view) {
+  if (!view.empty()) {
+    std::string str{view};
+    std::size_t pos = std::string_view::npos;
+    auto val = std::stoi(str, &pos, 10);
+    if (pos == view.size()) {
+      return val;
+    }
+  }
+  return std::nullopt;
+}
+std::optional<int> to_int(const llvm::Value& v) {
+  static const auto prefix = '%';
+  auto operand = get_operand(v);
+  std::string_view view{operand};
+  if (!view.empty() && view.at(0) == prefix) {
+    auto num = to_int(view.substr(1));
+    assert(num && 0 <= *num);
+    return *num;
+  }
+  return std::nullopt;
+}
+}  // namespace
 
 Cache::Cache() : cache_{std::make_unique<Super>()} {
   assert(cache_ && "failed cache init");
@@ -12,7 +38,7 @@ std::optional<int> Cache::lookup(const llvm::Value& reg) const {
   if (cache_) {
     if (auto it = cache_->find(&reg); it != cache_->end()) {
       return it->second;
-    } else if (auto num = register_number(reg)) {
+    } else if (auto num = to_int(reg)) {
       cache_->try_emplace(&reg, *num);
       return *num;
     }
