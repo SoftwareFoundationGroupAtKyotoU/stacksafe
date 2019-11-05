93e03ffefff9aae82adb31a99752618a46cb1083
hatsusato <knightspm5@gmail.com>
2019-11-05 18:00:22 +0900

    Use unordered_map for Blocks

--- src/blocks.hpp
+++ src/blocks.hpp
@@ -1,8 +1,8 @@
 #ifndef INCLUDE_GUARD_1C49E28A_FFB0_4196_8F04_3179A52F9485
 #define INCLUDE_GUARD_1C49E28A_FFB0_4196_8F04_3179A52F9485
 
-#include <map>
 #include <optional>
+#include <unordered_map>
 #include "env.hpp"
 #include "log.hpp"
 
@@ -13,8 +13,8 @@ class Function;
 
 namespace stacksafe {
 
-class Blocks : private std::map<const llvm::BasicBlock *, Env> {
-  using Super = std::map<const llvm::BasicBlock *, Env>;
+class Blocks : private std::unordered_map<const llvm::BasicBlock *, Env> {
+  using Super = std::unordered_map<const llvm::BasicBlock *, Env>;
   const Log log_;
 
  public:
