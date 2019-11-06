8dd6051a53d58fb6eb359baeb6dac7451e1766aa
hatsusato <knightspm5@gmail.com>
2019-11-06 17:34:04 +0900

    Arrange assrtions

--- src/interpret.cpp
+++ src/interpret.cpp
@@ -47,8 +47,9 @@ auto Interpreter::visitInstruction(llvm::Instruction &i) -> RetTy {
 }
 auto Interpreter::visitBinaryOperator(llvm::BinaryOperator &i) -> RetTy {
   auto lhs = i.getOperand(0);
+  assert(lhs && "invalid operand");
   auto rhs = i.getOperand(1);
-  assert(lhs && rhs && "invalid operand");
+  assert(rhs && "invalid operand");
   binop(i, *lhs, *rhs);
 }
 auto Interpreter::visitExtractElementInst(llvm::ExtractElementInst &i)
@@ -59,14 +60,16 @@ auto Interpreter::visitExtractElementInst(llvm::ExtractElementInst &i)
 }
 auto Interpreter::visitInsertElementInst(llvm::InsertElementInst &i) -> RetTy {
   auto lhs = i.getOperand(0);
+  assert(lhs && "invalid operand");
   auto rhs = i.getOperand(1);
-  assert(lhs && rhs && "invalid operand");
+  assert(rhs && "invalid operand");
   binop(i, *lhs, *rhs);
 }
 auto Interpreter::visitShuffleVectorInst(llvm::ShuffleVectorInst &i) -> RetTy {
   auto lhs = i.getOperand(0);
+  assert(lhs && "invalid operand");
   auto rhs = i.getOperand(1);
-  assert(lhs && rhs && "invalid operand");
+  assert(rhs && "invalid operand");
   binop(i, *lhs, *rhs);
 }
 auto Interpreter::visitExtractValue(llvm::ExtractValueInst &i) -> RetTy {
@@ -76,8 +79,9 @@ auto Interpreter::visitExtractValue(llvm::ExtractValueInst &i) -> RetTy {
 }
 auto Interpreter::visitInsertValue(llvm::InsertValueInst &i) -> RetTy {
   auto lhs = i.getAggregateOperand();
+  assert(lhs && "invalid operand");
   auto rhs = i.getInsertedValueOperand();
-  assert(lhs && rhs && "invalid operand");
+  assert(rhs && "invalid operand");
   binop(i, *lhs, *rhs);
 }
 auto Interpreter::visitAllocaInst(llvm::AllocaInst &i) -> RetTy {
@@ -90,20 +94,23 @@ auto Interpreter::visitLoadInst(llvm::LoadInst &i) -> RetTy {
 }
 auto Interpreter::visitStoreInst(llvm::StoreInst &i) -> RetTy {
   auto src = i.getValueOperand();
+  assert(src && "invalid operand");
   auto dst = i.getPointerOperand();
-  assert(src && dst && "invalid operand");
+  assert(dst && "invalid operand");
   store(*src, *dst);
 }
 auto Interpreter::visitAtomicCmpXchgInst(llvm::AtomicCmpXchgInst &i) -> RetTy {
   auto ptr = i.getPointerOperand();
+  assert(ptr && "invalid operand");
   auto val = i.getNewValOperand();
-  assert(ptr && val && "invalid operand");
+  assert(val && "invalid operand");
   cmpxchg(i, *ptr, *val);
 }
 auto Interpreter::visitAtomicRMWInst(llvm::AtomicRMWInst &i) -> RetTy {
   auto ptr = i.getPointerOperand();
+  assert(ptr && "invalid operand");
   auto val = i.getValOperand();
-  assert(ptr && val && "invalid operand");
+  assert(val && "invalid operand");
   cmpxchg(i, *ptr, *val);
 }
 auto Interpreter::visitGetElementPtrInst(llvm::GetElementPtrInst &i) -> RetTy {
