4b04b069a0ff28c966b0bea6f15431b9069b1fb0
hatsusato <knightspm5@gmail.com>
2019-11-06 11:01:02 +0900

    Add is_global

--- src/value.cpp
+++ src/value.cpp
@@ -1,7 +1,24 @@
 #include "value.hpp"
+#include <llvm/IR/Instructions.h>
 #include <functional>
 
 namespace stacksafe {
+namespace {
+bool is_global(const llvm::Constant* c) {
+  if (llvm::isa<llvm::GlobalValue>(c)) {
+    return true;
+  }
+  for (unsigned i = 0; i < c->getNumOperands(); ++i) {
+    assert(llvm::isa<llvm::Constant>(c->getOperand(i)) &&
+           "constant has non-constant");
+    if (auto v = llvm::dyn_cast<llvm::Constant>(c->getOperand(i));
+        is_global(v)) {
+      return true;
+    }
+  }
+  return false;
+}
+}  // namespace
 
 Value::Value(const llvm::Value* v) : val_{v} {}
 const llvm::Value* Value::value() const {
