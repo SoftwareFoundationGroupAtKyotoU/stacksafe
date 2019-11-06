e99563fe12aa94c7a9d8770b5d9646354a9b7e2a
hatsusato <knightspm5@gmail.com>
2019-11-06 17:50:28 +0900

    Add stacksafe_unreachable

--- src/utility.hpp
+++ src/utility.hpp
@@ -1,7 +1,12 @@
 #ifndef INCLUDE_GUARD_52E9C5D0_9006_4FB1_8F70_5B1B891C7DA4
 #define INCLUDE_GUARD_52E9C5D0_9006_4FB1_8F70_5B1B891C7DA4
 
+#include <llvm/Support/ErrorHandling.h>
 #include <string>
+
+#define stacksafe_unreachable(msg, val) \
+  llvm_unreachable(                     \
+      (std::string{"UNREACHABLE: " msg ": "} + to_str(val)).c_str())
 
 namespace stacksafe {
 class Value;
