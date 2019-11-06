79dc326af1cc681604afd3087655c5c825f2e0f5
hatsusato <knightspm5@gmail.com>
2019-11-06 17:50:18 +0900

    Add to_str for Value

--- src/utility.cpp
+++ src/utility.cpp
@@ -1,1 +1,16 @@
 #include "utility.hpp"
+#include <llvm/IR/Value.h>
+#include <llvm/Support/raw_ostream.h>
+#include "value.hpp"
+
+namespace stacksafe {
+
+std::string to_str(const Value& v) {
+  assert(v && "no operand at nullptr");
+  std::string buf;
+  llvm::raw_string_ostream stream{buf};
+  v.get()->printAsOperand(stream, false);
+  return stream.str();
+}
+
+}  // namespace stacksafe
--- src/utility.hpp
+++ src/utility.hpp
@@ -1,4 +1,13 @@
 #ifndef INCLUDE_GUARD_52E9C5D0_9006_4FB1_8F70_5B1B891C7DA4
 #define INCLUDE_GUARD_52E9C5D0_9006_4FB1_8F70_5B1B891C7DA4
 
+#include <string>
+
+namespace stacksafe {
+class Value;
+
+std::string to_str(const Value& v);
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_52E9C5D0_9006_4FB1_8F70_5B1B891C7DA4
