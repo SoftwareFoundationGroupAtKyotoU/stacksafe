16805783bdf6b8ea4e1d1603b209c66e2f135574
hatsusato <knightspm5@gmail.com>
2019-11-18 18:41:36 +0900

    Add MapRef

--- src/pool.cpp
+++ src/pool.cpp
@@ -1,1 +1,7 @@
 #include "pool.hpp"
+
+namespace stacksafe {
+
+MapRef::MapRef(const Map& m) : map_{&m} {}
+
+}  // namespace stacksafe
--- src/pool.hpp
+++ src/pool.hpp
@@ -1,4 +1,14 @@
 #ifndef INCLUDE_GUARD_721DAB69_1C96_4A4D_BE1C_1C8B66A7065E
 #define INCLUDE_GUARD_721DAB69_1C96_4A4D_BE1C_1C8B66A7065E
 
+namespace stacksafe {
+class Map;
+
+class MapRef {
+  const Map* map_;
+  explicit MapRef(const Map& m);
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_721DAB69_1C96_4A4D_BE1C_1C8B66A7065E
