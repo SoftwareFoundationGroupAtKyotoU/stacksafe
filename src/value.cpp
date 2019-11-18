aec52e8a242180bdf0a7aef197f7612255a48f58
hatsusato <knightspm5@gmail.com>
2019-11-18 16:38:28 +0900

    Remove Value::Kind

--- src/value.cpp
+++ src/value.cpp
@@ -1,9 +1,6 @@
 #include "value.hpp"
-#include <llvm/IR/Instructions.h>
-#include "utility.hpp"
 
 namespace stacksafe {
-namespace {}  // namespace
 
 Value::Value() : val_{nullptr} {}
 Value::Value(const llvm::Value &v) : val_{&v} {}
@@ -15,20 +12,6 @@ const llvm::Value *Value::get() const {
 }
 std::size_t Value::hash() const {
   return std::hash<const llvm::Value *>{}(val_);
-}
-auto Value::kind() const -> Kind {
-  const auto v = get();
-  if (!v) {
-    return Kind::GLOBAL;
-  } else if (is_argument(*v)) {
-    return Kind::ARGUMENT;
-  } else if (auto c = llvm::dyn_cast<llvm::Constant>(v)) {
-    return is_global(*c) ? Kind::GLOBAL : Kind::CONSTANT;
-  } else if (auto i = llvm::dyn_cast<llvm::Instruction>(v)) {
-    return is_register(*i) ? Kind::REGISTER : Kind::OTHER;
-  } else {
-    return Kind::OTHER;
-  }
 }
 bool operator<(const Value &lhs, const Value &rhs) {
   return lhs.get() < rhs.get();
