9d9ac18dd012b3ec6dc2aff1addddb1dc6952c58
hatsusato <knightspm5@gmail.com>
2019-11-19 15:55:40 +0900

    Add MapsTo

--- src/flat.hpp
+++ src/flat.hpp
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_GUARD_9AD1204C_8632_4047_8AC2_467F02D9DFF8
 #define INCLUDE_GUARD_9AD1204C_8632_4047_8AC2_467F02D9DFF8
 
+#include "symbol.hpp"
+#include "value.hpp"
+
+namespace stacksafe {
+
+class MapsTo : private std::tuple<Value, Symbol> {
+  using Super = std::tuple<Value, Symbol>;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_9AD1204C_8632_4047_8AC2_467F02D9DFF8
