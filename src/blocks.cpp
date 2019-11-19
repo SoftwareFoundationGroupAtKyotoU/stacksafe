4b6781b74626512ff8ea08c1356fd9a39abcdbf1
hatsusato <knightspm5@gmail.com>
2019-11-19 19:46:28 +0900

    Remove Blocks::merge

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -11,9 +11,6 @@ Blocks::Blocks(const llvm::Function &f) {
 Env Blocks::add(const FlatEnv &env) {
   return flat_.add(env);
 }
-void Blocks::merge(const llvm::BasicBlock &dst, const FlatEnv &src) {
-  get(dst).merge(flat_.add(src));
-}
 const Env &Blocks::get(const llvm::BasicBlock &b) const {
   auto it = Super::find(&b);
   assert(it != Super::end() && "unknown basicblock");
