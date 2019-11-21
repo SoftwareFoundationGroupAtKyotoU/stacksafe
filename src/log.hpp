7ddc553fc7322a480329517210fc5c493a312c08
hatsusato <knightspm5@gmail.com>
2019-11-21 12:52:38 +0900

    Update print_stack

--- src/log.hpp
+++ src/log.hpp
@@ -37,7 +37,7 @@ class Log {
   void print(const llvm::BasicBlock &b) const;
   void print_heap(const Symbol &key, const Domain &val,
                   const Domain &add) const;
-  void print_stack(const Value &key, const Domain &val,
+  void print_stack(const llvm::Instruction &key, const Domain &val,
                    const Domain &add) const;
   void print(const Error &err) const;
 };
