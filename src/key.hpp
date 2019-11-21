96be520aeb17ce565a437b7da3bba14a06800c26
hatsusato <knightspm5@gmail.com>
2019-11-21 14:00:55 +0900

    Add Key

--- src/key.hpp
+++ src/key.hpp
@@ -1,4 +1,21 @@
 #ifndef INCLUDE_GUARD_EE020F3B_04D6_40F2_943A_DF1081556287
 #define INCLUDE_GUARD_EE020F3B_04D6_40F2_943A_DF1081556287
 
+#include <cstdint>
+
+namespace llvm {
+class Value;
+}
+
+namespace stacksafe {
+
+class Key {
+  union {
+    const llvm::Value* val_;
+    std::uintptr_t sym_;
+  };
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_EE020F3B_04D6_40F2_943A_DF1081556287
