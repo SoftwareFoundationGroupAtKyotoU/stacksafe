9ef2f69269a2fc0c5da7fc26b37eadd9829e1633
hatsusato <knightspm5@gmail.com>
2019-11-21 11:31:24 +0900

    Add ParamIterator::operator*

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -27,6 +27,7 @@ class Params : private std::unordered_set<const llvm::Value *> {
    public:
     using Iter::iterator_category, Iter::value_type, Iter::difference_type,
         Iter::pointer, Iter::reference;
+    const llvm::Value &operator*() const { return *Iter::operator*(); }
   };
 
  public:
