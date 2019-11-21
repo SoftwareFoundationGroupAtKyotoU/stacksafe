04a1272bc569ada42d064d460c44c452a3695da2
hatsusato <knightspm5@gmail.com>
2019-11-21 18:12:06 +0900

    Separate Params

--- src/params.cpp
+++ src/params.cpp
@@ -1,1 +1,36 @@
 #include "params.hpp"
+
+namespace stacksafe {
+
+Params::ParamsIterator::ParamsIterator(Base it) : Base{it} {}
+auto Params::ParamsIterator::base() -> Base & {
+  return *this;
+}
+auto Params::ParamsIterator::base() const -> const Base & {
+  return *this;
+}
+const llvm::Value &Params::ParamsIterator::operator*() const {
+  return **base();
+}
+auto Params::ParamsIterator::operator++() -> ParamsIterator & {
+  ++base();
+  return *this;
+}
+bool Params::ParamsIterator::operator==(ParamsIterator it) const {
+  return base() == it.base();
+}
+bool Params::ParamsIterator::operator!=(ParamsIterator it) const {
+  return base() != it.base();
+}
+
+auto Params::begin() const -> ParamsIterator {
+  return ParamsIterator{Super::begin()};
+}
+auto Params::end() const -> ParamsIterator {
+  return ParamsIterator{Super::end()};
+}
+void Params::emplace(const llvm::Value &v) {
+  Super::emplace(&v);
+}
+
+}  // namespace stacksafe
--- src/params.hpp
+++ src/params.hpp
@@ -8,6 +8,7 @@ class Value;
 }
 
 namespace stacksafe {
+
 class Params : private std::unordered_set<const llvm::Value *> {
   using Super = std::unordered_set<const llvm::Value *>;
   class ParamsIterator;
@@ -18,34 +19,22 @@ class Params : private std::unordered_set<const llvm::Value *> {
   void emplace(const llvm::Value &v);
 };
 
-class Params::ParamsIterator : private Super::const_iterator {
+class Params::ParamsIterator : private Params::Super::const_iterator {
   friend class Params;
   using Base = Super::const_iterator;
-  explicit ParamsIterator(Base it) : Base{it} {}
-  Base &base() { return *this; }
-  const Base &base() const { return *this; }
+  explicit ParamsIterator(Base it);
+  Base &base();
+  const Base &base() const;
 
  public:
   using Base::iterator_category, Base::value_type, Base::difference_type,
       Base::pointer, Base::reference;
-  const llvm::Value &operator*() const { return **base(); }
-  ParamsIterator &operator++() {
-    ++base();
-    return *this;
-  }
-  bool operator==(ParamsIterator it) const { return base() == it.base(); }
-  bool operator!=(ParamsIterator it) const { return base() != it.base(); }
+  const llvm::Value &operator*() const;
+  ParamsIterator &operator++();
+  bool operator==(ParamsIterator it) const;
+  bool operator!=(ParamsIterator it) const;
 };
 
-auto Params::begin() const -> ParamsIterator {
-  return ParamsIterator{Super::begin()};
-}
-auto Params::end() const -> ParamsIterator {
-  return ParamsIterator{Super::end()};
-}
-void Params::emplace(const llvm::Value &v) {
-  Super::emplace(&v);
-}
 }  // namespace stacksafe
 
 #endif  // INCLUDE_GUARD_7033D3E3_92CE_493B_862B_265AB050E953
