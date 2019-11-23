5bc1e3d20b0ef25c3aa9a7cb62f4f8d4d165efa6
hatsusato <knightspm5@gmail.com>
2019-11-23 12:42:24 +0900

    Add Env

--- src/env.hpp
+++ src/env.hpp
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_GUARD_DA7CDD8A_EA48_45CA_B057_F470D4F72F4E
 #define INCLUDE_GUARD_DA7CDD8A_EA48_45CA_B057_F470D4F72F4E
 
+#include <unordered_map>
+#include "map.hpp"
+#include "value.hpp"
+
+namespace stacksafe {
+
+class Env : private std::unordered_multimap<Value, MapRef> {
+  using Super = std::unordered_multimap<Value, MapRef>;
+};
+
+}  // namespace stacksafe
 #endif  // INCLUDE_GUARD_DA7CDD8A_EA48_45CA_B057_F470D4F72F4E
