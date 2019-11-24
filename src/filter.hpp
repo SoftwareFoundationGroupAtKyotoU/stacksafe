4d505cdee2c861d1d904c68af6893f7fb70880a3
hatsusato <knightspm5@gmail.com>
2019-11-24 14:34:03 +0900

    Add BloomFilter

--- src/filter.hpp
+++ src/filter.hpp
@@ -1,7 +1,9 @@
 #ifndef INCLUDE_GUARD_98FEBCB3_4573_41C9_AA40_98728267AB74
 #define INCLUDE_GUARD_98FEBCB3_4573_41C9_AA40_98728267AB74
 
+#include <bitset>
 #include <climits>
+#include <vector>
 
 #define _USE_MATH_DEFINES
 #include <cmath>
@@ -13,6 +15,11 @@ constexpr std::size_t block_length(std::size_t num_of_hash) {
   const auto factor = static_cast<double>(num_of_hash) / M_LN2 / CHAR_BIT;
   return static_cast<std::size_t>(factor) * CHAR_BIT;
 }
+
+class BloomFilter {
+  using Block = std::bitset<block_length(10)>;
+  std::vector<Block> buf_;
+};
 
 }  // namespace stacksafe
 
