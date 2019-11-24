a24597dd18dcfb21896c2d2c6125c6a7997ff5b1
hatsusato <knightspm5@gmail.com>
2019-11-24 12:49:29 +0900

    Update MapPool::add

--- src/pool.cpp
+++ src/pool.cpp
@@ -5,6 +5,11 @@
 #include "map.hpp"
 
 namespace stacksafe {
+namespace {
+bool compare(const MapPtr &lhs, const MapPtr &rhs) {
+  return hash_value(lhs.get()) < hash_value(rhs.get());
+}
+}  // namespace
 
 MapPtr::MapPtr(const Map &map) : Super{std::make_unique<Map>(map)} {}
 MapPtr::MapPtr(MapPtr &&) = default;
@@ -19,18 +24,14 @@ Map &MapPtr::get() {
 
 MapRef MapPool::add(const Map &map) {
   MapPtr ptr{map};
-  const auto compare = [](const MapPtr &lhs, const MapPtr &rhs) {
-    return hash_value(lhs.get()) < hash_value(rhs.get());
-  };
   const auto [lb, ub] = std::equal_range(begin(), end(), ptr, compare);
-  auto it = lb;
-  for (; it != ub; ++it) {
+  for (auto it = lb; it != ub; ++it) {
     if (Map::equals(it->get(), map)) {
       return MapRef{it->get()};
     }
   }
   MapRef ref{ptr.get()};
-  Super::insert(it, std::move(ptr));
+  Super::insert(ub, std::move(ptr));
   return ref;
 }
 
