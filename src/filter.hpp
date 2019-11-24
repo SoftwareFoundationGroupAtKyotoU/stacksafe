e84d54aa183d2c3c3de941adcf83ffe97154e762
hatsusato <knightspm5@gmail.com>
2019-11-24 14:33:29 +0900

    Add block_length

--- src/filter.hpp
+++ src/filter.hpp
@@ -1,4 +1,19 @@
 #ifndef INCLUDE_GUARD_98FEBCB3_4573_41C9_AA40_98728267AB74
 #define INCLUDE_GUARD_98FEBCB3_4573_41C9_AA40_98728267AB74
 
+#include <climits>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#undef _USE_MATH_DEFINES
+
+namespace stacksafe {
+
+constexpr std::size_t block_length(std::size_t num_of_hash) {
+  const auto factor = static_cast<double>(num_of_hash) / M_LN2 / CHAR_BIT;
+  return static_cast<std::size_t>(factor) * CHAR_BIT;
+}
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_98FEBCB3_4573_41C9_AA40_98728267AB74
