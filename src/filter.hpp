a8cbd95f2cdea8c1eacd0b6d7d81db93e8776379
hatsusato <knightspm5@gmail.com>
2019-11-24 15:04:07 +0900

    Update block_length to block_width

--- src/filter.hpp
+++ src/filter.hpp
@@ -11,13 +11,15 @@
 
 namespace stacksafe {
 
-constexpr std::size_t block_length(std::size_t num_of_hash) {
-  const auto factor = static_cast<double>(num_of_hash) / M_LN2 / CHAR_BIT;
+constexpr std::size_t block_width(std::size_t k) {
+  const auto factor = static_cast<double>(k) / M_LN2 / CHAR_BIT;
   return static_cast<std::size_t>(factor) * CHAR_BIT;
 }
 
 class BloomFilter {
-  using Block = std::bitset<block_length(10)>;
+  static constexpr auto num_of_hash = 10;
+  static constexpr auto width = block_width(num_of_hash);
+  using Block = std::bitset<width>;
   std::vector<Block> buf_;
 
  public:
