7fe91bc91a12c77d98789c43958149d59dedc667
hatsusato <knightspm5@gmail.com>
2019-11-25 09:47:12 +0900

    Add Node

--- src/zdd.hpp
+++ src/zdd.hpp
@@ -1,4 +1,18 @@
 #ifndef INCLUDE_GUARD_6F13AE33_4EC4_4948_AE33_FF20C0E66402
 #define INCLUDE_GUARD_6F13AE33_4EC4_4948_AE33_FF20C0E66402
 
+#include <memory>
+#include "value.hpp"
+
+namespace stacksafe {
+class Node;
+using NodePtr = std::unique_ptr<Node>;
+
+class Node {
+  Pair level_;
+  NodePtr lo_, hi_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_6F13AE33_4EC4_4948_AE33_FF20C0E66402
