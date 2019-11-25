8a3f44427045faebe1109f0f1cb8d33b976685cb
hatsusato <knightspm5@gmail.com>
2019-11-25 16:49:29 +0900

    Add Scc

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,4 +1,18 @@
 #ifndef INCLUDE_GUARD_E1E5ACD3_3435_4167_A6B2_0E8D6A2A38AC
 #define INCLUDE_GUARD_E1E5ACD3_3435_4167_A6B2_0E8D6A2A38AC
 
+#include <vector>
+
+namespace llvm {
+class BasicBlock;
+}
+
+namespace stacksafe {
+
+class Scc : private std::vector<const llvm::BasicBlock*> {
+  using Super = std::vector<const llvm::BasicBlock*>;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_E1E5ACD3_3435_4167_A6B2_0E8D6A2A38AC
