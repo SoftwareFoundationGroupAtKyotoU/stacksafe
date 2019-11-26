b2fe24fc316f244a651d7e35ebb20c5d4368e8a1
hatsusato <knightspm5@gmail.com>
2019-11-26 18:07:23 +0900

    Remove Scc

--- src/graph.hpp
+++ src/graph.hpp
@@ -86,25 +86,6 @@ class Tarjan {
   Frame& map(const Block& b);
 };
 
-class Scc : private std::vector<const llvm::BasicBlock*> {
-  using Super = std::vector<const llvm::BasicBlock*>;
-  using Set = std::set<const llvm::BasicBlock*>;
-  std::set<SccPtr> succ_;
-  Map map_;
-
- public:
-  using Stack = std::stack<SccPtr, std::vector<SccPtr>>;
-  using Super::begin, Super::end, Super::emplace_back;
-  bool contains(const llvm::BasicBlock* b) const;
-  bool is_loop() const;
-  Set out_degree() const;
-  void add_successor(const SccPtr& ptr);
-  void merge(const Map& map);
-  void convey();
-  Map& map();
-  static Stack decompose(const llvm::Function& f);
-};
-
 }  // namespace stacksafe
 
 #endif  // INCLUDE_GUARD_E1E5ACD3_3435_4167_A6B2_0E8D6A2A38AC
