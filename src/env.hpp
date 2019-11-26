ba2387e7e21b6f4bf104663f8064e9483d50adbb
hatsusato <knightspm5@gmail.com>
2019-11-26 13:21:20 +0900

    Remove Env

--- src/env.hpp
+++ src/env.hpp
@@ -13,32 +13,5 @@ class Function;
 
 namespace stacksafe {
 class MapPool;
-
-class Env : private std::unordered_multimap<Value, MapRef> {
-  friend class MapPool;
-  using Super = std::unordered_multimap<Value, MapRef>;
-  ZddPtr zdd_;
-  BloomFilter filter_;
-
- public:
-  bool includes(const Env& env) const;
-  void merge(const Env& env);
-
- protected:
-  explicit Env(std::size_t count);
-  void insert(const MapRef& ref);
-  Domain lookup(const Value& key) const;
-  void update(Map& map, const Value& key, const Domain& dom) const;
-
- private:
-  void insert(const Value& key, const MapRef& ref);
-  bool includes(const MapRef& ref) const;
-  bool contains(const Value& key, const Value& val) const;
-  static bool range_contains(const_iterator lb, const_iterator ub,
-                             const Value& val);
-  static bool range_contains(const_iterator lb, const_iterator ub,
-                             const MapRef& ref);
-};
-
 }  // namespace stacksafe
 #endif  // INCLUDE_GUARD_DA7CDD8A_EA48_45CA_B057_F470D4F72F4E
