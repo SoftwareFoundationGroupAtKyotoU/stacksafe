30b661891409777661cb2106cfda86f5ce568357
hatsusato <knightspm5@gmail.com>
2019-11-29 15:16:35 +0900

    Add Depend

--- src/depend.hpp
+++ src/depend.hpp
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_GUARD_DA5A1318_1109_4382_9A1C_7278AB44F4FA
 #define INCLUDE_GUARD_DA5A1318_1109_4382_9A1C_7278AB44F4FA
 
+#include <cstdint>
+#include <vector>
+
+namespace stacksafe {
+
+class Depend : private std::vector<std::uint8_t> {
+  using Super = std::vector<std::uint8_t>;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_DA5A1318_1109_4382_9A1C_7278AB44F4FA
