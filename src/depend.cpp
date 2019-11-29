3ec3cb5e817073f0d542c94fd6d513ab571da7d6
hatsusato <knightspm5@gmail.com>
2019-11-29 15:25:28 +0900

    Update Depend::Depend

--- src/depend.cpp
+++ src/depend.cpp
@@ -2,7 +2,11 @@
 
 namespace stacksafe {
 
-Depend::Depend(unsigned n) : Super((n + 2) * n), size_{n} {}
+Depend::Depend(unsigned n) : Super((n + 2) * n), size_{n} {
+  for (unsigned i = 0; i < n; ++i) {
+    set(i, i);
+  }
+}
 void Depend::set(unsigned from, unsigned to) {
   Super::at((size_ + 2) * from + to) = 1;
 }
