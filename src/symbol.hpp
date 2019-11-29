50e11866684a83f193e47230a09db8d79d452de6
hatsusato <knightspm5@gmail.com>
2019-11-29 10:13:12 +0900

    Add Symbol

--- src/symbol.hpp
+++ src/symbol.hpp
@@ -1,4 +1,14 @@
 #ifndef INCLUDE_GUARD_FAE8D5CB_240E_4E65_B38A_B29E5FF091CE
 #define INCLUDE_GUARD_FAE8D5CB_240E_4E65_B38A_B29E5FF091CE
 
+#include <cstdint>
+
+namespace stacksafe {
+
+class Symbol {
+  std::uintptr_t sym_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_FAE8D5CB_240E_4E65_B38A_B29E5FF091CE
