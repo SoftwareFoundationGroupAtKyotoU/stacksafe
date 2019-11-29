5e2f7e6c661c2f32f5af1bc5f2f2a4fedd01092c
hatsusato <knightspm5@gmail.com>
2019-11-29 11:09:36 +0900

    Update to_symbol

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -1,18 +1,20 @@
 #include "symbol.hpp"
 #include <llvm/IR/Argument.h>
-#include <cassert>
 #include "utility.hpp"
 
 namespace stacksafe {
 namespace {
 constexpr std::uintptr_t symbol_flag = 0x1;
 std::uintptr_t to_symbol(const void* p) {
-  assert(p);
-  const auto val = reinterpret_cast<std::uintptr_t>(p);
-  if (val == 0) {
-    fatal_error("nullptr representation may conflict");
+  if (p) {
+    const auto val = reinterpret_cast<std::uintptr_t>(p);
+    if (val == 0) {
+      fatal_error("nullptr representation may conflict");
+    }
+    return val | symbol_flag;
+  } else {
+    return symbol_flag;
   }
-  return val | symbol_flag;
 }
 const llvm::Value* to_pointer(std::uintptr_t u) {
   const auto p = u & ~symbol_flag;
@@ -29,11 +31,11 @@ Symbol::Symbol(const void* p) : sym_{to_symbol(p)} {
     fatal_error("Value representation may conflict");
   }
 }
-Symbol::Symbol() : sym_{symbol_flag} {}
+Symbol::Symbol() : sym_{to_symbol(nullptr)} {}
 Symbol::Symbol(const llvm::AllocaInst& i) : Symbol{&i} {}
 Symbol::Symbol(const llvm::Argument& a) : Symbol{&a} {}
 bool Symbol::is_global() const {
-  return sym_ == symbol_flag;
+  return to_pointer(sym_) == nullptr;
 }
 const llvm::Argument* Symbol::as_argument() const {
   return llvm::dyn_cast_or_null<llvm::Argument>(to_pointer(sym_));
