0089e50ffb165875a049554d34ef1ee9b0f5af91
hatsusato <knightspm5@gmail.com>
2019-11-29 09:39:29 +0900

    Add Register

--- src/register.hpp
+++ src/register.hpp
@@ -1,4 +1,16 @@
 #ifndef INCLUDE_GUARD_E89E0FBA_AE75_4472_9027_FDC32CA21954
 #define INCLUDE_GUARD_E89E0FBA_AE75_4472_9027_FDC32CA21954
 
+namespace llvm {
+class Value;
+}
+
+namespace stacksafe {
+
+class Register {
+  const llvm::Value* reg_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_E89E0FBA_AE75_4472_9027_FDC32CA21954
