520e2e5baa080e478996c5272fac30a8e1808a63
hatsusato <knightspm5@gmail.com>
2019-11-29 10:33:11 +0900

    Update Register::Register

--- src/register.cpp
+++ src/register.cpp
@@ -1,21 +1,18 @@
 #include "register.hpp"
 #include <llvm/IR/Argument.h>
 #include <llvm/IR/Instruction.h>
-#include <cstdlib>
 #include "utility.hpp"
 
 namespace stacksafe {
 
 Register::Register(const llvm::Value* v) : reg_{v} {
   if (least_significant_bit(reg_)) {
-    debug::print("ERROR: Value representation may conflict");
-    std::exit(EXIT_FAILURE);
+    fatal_error("Value representation may conflict");
   }
 }
 Register::Register(const llvm::Instruction& i) : Register{&i} {
   if (!is_register(i)) {
-    debug::print("ERROR: invalid register");
-    std::exit(EXIT_FAILURE);
+    fatal_error("invalid register");
   }
 }
 Register::Register(const llvm::Argument& a) : Register{&a} {}
