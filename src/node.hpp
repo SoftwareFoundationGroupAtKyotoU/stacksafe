6a5406a405d63a278d04edf8615855abff50e7d1
hatsusato <knightspm5@gmail.com>
2020-02-16 12:40:28 +0900

    Add Node

--- src/node.hpp
+++ src/node.hpp
@@ -1,4 +1,16 @@
 #ifndef INCLUDE_GUARD_A9F529C8_A504_4663_8ADD_A1FC022EA7B5
 #define INCLUDE_GUARD_A9F529C8_A504_4663_8ADD_A1FC022EA7B5
 
+#include <variant>
+#include "register.hpp"
+#include "symbol.hpp"
+
+namespace stacksafe {
+
+class Node : std::variant<Symbol, Register> {
+  using Super = std::variant<Symbol, Register>;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_A9F529C8_A504_4663_8ADD_A1FC022EA7B5
