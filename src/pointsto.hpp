91c630392b215247556e0b595ce7fd20cc581c23
hatsusato <knightspm5@gmail.com>
2020-02-24 17:08:22 +0900

    Add PointsTo

--- src/pointsto.hpp
+++ src/pointsto.hpp
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_GUARD_E3BE8F38_E950_4162_9837_04D50D8E97C6
 #define INCLUDE_GUARD_E3BE8F38_E950_4162_9837_04D50D8E97C6
 
+#include <llvm/IR/InstVisitor.h>
+
+namespace stacksafe {
+
+class PointsTo : public llvm::InstVisitor<PointsTo, void> {
+  using RetTy = void;
+  using Super = llvm::InstVisitor<PointsTo, RetTy>;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_E3BE8F38_E950_4162_9837_04D50D8E97C6
