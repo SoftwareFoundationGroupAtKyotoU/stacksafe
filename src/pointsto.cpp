748e10d38d8af61654c02acd8de783d3ae80b3d9
hatsusato <knightspm5@gmail.com>
2020-02-24 17:48:37 +0900

    Add PointsTo utilities

--- src/pointsto.cpp
+++ src/pointsto.cpp
@@ -1,11 +1,55 @@
 #include "pointsto.hpp"
 #include <llvm/Support/Debug.h>
 #include "graph.hpp"
+#include "params.hpp"
 #include "utility.hpp"
 
 namespace stacksafe {
 
 PointsTo::PointsTo(Graph &g) : graph_{g}, updated_{false} {}
+void PointsTo::binop(const llvm::Instruction &dst, const llvm::Value &lhs,
+                     const llvm::Value &rhs) {
+  NodeSet heads;
+  heads.merge(lookup(lhs));
+  heads.merge(lookup(rhs));
+  append(dst, heads);
+}
+void PointsTo::alloc(const llvm::AllocaInst &dst) {
+  NodeSet heads;
+  heads.insert(Node{Symbol{dst}});
+  append(dst, heads);
+}
+void PointsTo::load(const llvm::Instruction &dst, const llvm::Value &src) {
+  NodeSet heads;
+  for (const auto &sym : lookup(src)) {
+    heads.merge(graph_.heads(sym));
+  }
+  append(dst, heads);
+}
+void PointsTo::store(const llvm::Value &src, const llvm::Value &dst) {
+  const auto heads = lookup(src);
+  for (const auto &tail : lookup(dst)) {
+    for (const auto &head : heads) {
+      update(graph_.append(tail, head));
+    }
+  }
+}
+void PointsTo::cmpxchg(const llvm::Instruction &dst, const llvm::Value &ptr,
+                       const llvm::Value &val) {
+  load(dst, ptr);
+  store(val, ptr);
+}
+void PointsTo::cast(const llvm::Instruction &dst, const llvm::Value &src) {
+  append(dst, lookup(src));
+}
+void PointsTo::phi(const llvm::Instruction &dst, const Params &params) {
+  NodeSet heads;
+  for (const auto &arg : params) {
+    heads.merge(lookup(arg));
+  }
+  append(dst, heads);
+}
+void PointsTo::constant(const llvm::Instruction &) {}
 NodeSet PointsTo::lookup(const Symbol &tail) const {
   return graph_.heads(Node{tail});
 }
--- src/pointsto.hpp
+++ src/pointsto.hpp
@@ -6,6 +6,7 @@
 namespace stacksafe {
 class Graph;
 class NodeSet;
+class Params;
 class Symbol;
 
 class PointsTo : public llvm::InstVisitor<PointsTo, void> {
@@ -16,6 +17,18 @@ class PointsTo : public llvm::InstVisitor<PointsTo, void> {
 
  public:
   explicit PointsTo(Graph &g);
+
+ private:
+  void binop(const llvm::Instruction &dst, const llvm::Value &lhs,
+             const llvm::Value &rhs);
+  void alloc(const llvm::AllocaInst &dst);
+  void load(const llvm::Instruction &dst, const llvm::Value &src);
+  void store(const llvm::Value &src, const llvm::Value &dst);
+  void cmpxchg(const llvm::Instruction &dst, const llvm::Value &ptr,
+               const llvm::Value &val);
+  void cast(const llvm::Instruction &dst, const llvm::Value &src);
+  void phi(const llvm::Instruction &dst, const Params &params);
+  void constant(const llvm::Instruction &dst);
 
  private:
   NodeSet lookup(const Symbol &tail) const;
