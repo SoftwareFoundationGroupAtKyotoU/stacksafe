02a29c422f26c90d42c7a7458ebb6c125deb1eea
hatsusato <knightspm5@gmail.com>
2020-02-25 13:20:09 +0900

    Add Effect

--- src/effect.hpp
+++ src/effect.hpp
@@ -1,4 +1,14 @@
 #ifndef INCLUDE_GUARD_EB0B0AEA_791C_48A0_BE6F_8E63AC0A1D93
 #define INCLUDE_GUARD_EB0B0AEA_791C_48A0_BE6F_8E63AC0A1D93
 
+#include "matrix.hpp"
+
+namespace stacksafe {
+
+class Effect {
+  Matrix mat_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_EB0B0AEA_791C_48A0_BE6F_8E63AC0A1D93
