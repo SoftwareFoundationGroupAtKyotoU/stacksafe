40a514193bfe77ee2993fb39cc8a85690a8c3779
hatsusato <knightspm5@gmail.com>
2020-02-25 15:07:34 +0900

    Add EffectLine::to_size_t

--- src/effect.cpp
+++ src/effect.cpp
@@ -1,4 +1,7 @@
 #include "effect.hpp"
+#include <climits>
+#include <cstdlib>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -12,6 +15,7 @@ class EffectLine {
 
  private:
   static Views split(std::string_view v, const char* delim);
+  static std::optional<std::size_t> to_size_t(std::string_view v);
 };
 auto EffectLine::split(std::string_view v, const char* delim) -> Views {
   Views views;
@@ -23,6 +27,19 @@ auto EffectLine::split(std::string_view v, const char* delim) -> Views {
   views.emplace_back(v);
   return views;
   ;
+}
+std::optional<std::size_t> EffectLine::to_size_t(std::string_view v) {
+  std::string buf{v};
+  const auto b = buf.c_str();
+  const auto e = b + buf.size();
+  if (b != e) {
+    char* p = nullptr;
+    const auto val = std::strtol(b, &p, 10);
+    if (p == e && 0 <= val && val != LONG_MAX) {
+      return val;
+    }
+  }
+  return std::nullopt;
 }
 }  // namespace
 }  // namespace stacksafe
