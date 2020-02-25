5c1af988f219af75648d5b920c72edb061005718
hatsusato <knightspm5@gmail.com>
2020-02-25 14:57:32 +0900

    Move split to EffectLine

--- src/effect.cpp
+++ src/effect.cpp
@@ -5,19 +5,24 @@
 
 namespace stacksafe {
 namespace {
-std::vector<std::string_view> split(std::string_view v, const char* delim) {
-  std::vector<std::string_view> vec;
-  const auto next = [&v, delim]() { return v.find_first_of(delim); };
-  for (auto pos = next(); pos != v.npos; pos = next()) {
-    vec.emplace_back(v.substr(0, pos));
-    v.remove_prefix(pos + 1);
-  }
-  vec.emplace_back(v);
-  return vec;
-}
 class EffectLine {
+  using Views = std::vector<std::string_view>;
   std::string name_;
   std::size_t arity_;
+
+ private:
+  static Views split(std::string_view v, const char* delim);
 };
+auto EffectLine::split(std::string_view v, const char* delim) -> Views {
+  Views views;
+  const auto next = [&v, delim]() { return v.find_first_of(delim); };
+  for (auto pos = next(); pos != v.npos; pos = next()) {
+    views.emplace_back(v.substr(0, pos));
+    v.remove_prefix(pos + 1);
+  }
+  views.emplace_back(v);
+  return views;
+  ;
+}
 }  // namespace
 }  // namespace stacksafe
