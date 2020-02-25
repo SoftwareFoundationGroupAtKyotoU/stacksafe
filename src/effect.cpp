896fe067f9babb68e2f81c5cfb201ad93d0465ed
hatsusato <knightspm5@gmail.com>
2020-02-25 13:32:14 +0900

    Add split

--- src/effect.cpp
+++ src/effect.cpp
@@ -1,1 +1,18 @@
 #include "effect.hpp"
+#include <string_view>
+#include <vector>
+
+namespace stacksafe {
+namespace {
+std::vector<std::string_view> split(std::string_view v, const char* delim) {
+  std::vector<std::string_view> vec;
+  const auto next = [&v, delim]() { return v.find_first_of(delim); };
+  for (auto pos = next(); pos != v.npos; pos = next()) {
+    vec.emplace_back(v.substr(0, pos));
+    v.remove_prefix(pos + 1);
+  }
+  vec.emplace_back(v);
+  return vec;
+}
+}  // namespace
+}  // namespace stacksafe
