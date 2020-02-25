d01a0aa802caa80fd2ecbe0a559bd46c160e2553
hatsusato <knightspm5@gmail.com>
2020-02-25 19:51:45 +0900

    Add Arity::to_index

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -30,6 +30,7 @@ class Arity : private Index {
   std::size_t convert(Index index) const;
   bool is_valid(Index index) const;
   Index to_index(std::string_view v) const;
+  Index to_index(std::size_t i) const;
 };
 
 class Matrix : private std::vector<std::uint8_t> {
