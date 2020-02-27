00433431dcec38b0020fe6469df4c0471f52dacf
hatsusato <knightspm5@gmail.com>
2020-02-27 16:39:35 +0900

    Register StackSafe

--- pass/stacksafe.cpp
+++ pass/stacksafe.cpp
@@ -1,8 +1,14 @@
 #include "stacksafe.hpp"
 
 namespace stacksafe {
+namespace {
+constexpr auto stacksafe{"stacksafe"};
+}
 
 char StackSafe::ID = 0;
 StackSafe::StackSafe() : llvm::ModulePass{ID} {}
+
+static llvm::RegisterPass<StackSafe> registerpass(stacksafe, stacksafe, false,
+                                                  true);
 
 }  // namespace stacksafe
