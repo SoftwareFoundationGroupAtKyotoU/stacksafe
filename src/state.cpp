59fef78accf94c79e2c3575bb939a05ce0ac52cc
hatsusato <knightspm5@gmail.com>
2020-02-27 11:33:13 +0900

    Add Component

--- src/state.cpp
+++ src/state.cpp
@@ -1,1 +1,7 @@
 #include "state.hpp"
+
+namespace stacksafe {
+
+Component::Component(const Blocks& b) : blocks_{b}, graph_{} {}
+
+}  // namespace stacksafe
--- src/state.hpp
+++ src/state.hpp
@@ -1,4 +1,19 @@
 #ifndef INCLUDE_GUARD_F20B95D3_9559_4A85_99D6_241914BC4F14
 #define INCLUDE_GUARD_F20B95D3_9559_4A85_99D6_241914BC4F14
 
+#include "block.hpp"
+#include "graph.hpp"
+
+namespace stacksafe {
+
+class Component {
+  Blocks blocks_;
+  Graph graph_;
+
+ public:
+  explicit Component(const Blocks& b);
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_F20B95D3_9559_4A85_99D6_241914BC4F14
