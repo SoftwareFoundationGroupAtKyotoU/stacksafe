ae2699b0a46991f69009cd6fef41422374edb107
hatsusato <knightspm5@gmail.com>
2020-02-27 14:57:19 +0900

    Add DataFlow::state_

--- src/dataflow.hpp
+++ src/dataflow.hpp
@@ -1,6 +1,8 @@
 #ifndef INCLUDE_GUARD_25E5557B_880B_4E38_B068_6B33D855AA6E
 #define INCLUDE_GUARD_25E5557B_880B_4E38_B068_6B33D855AA6E
 
+#include "state.hpp"
+
 namespace llvm {
 class Function;
 }
@@ -9,6 +11,7 @@ namespace stacksafe {
 
 class DataFlow {
   const llvm::Function& func_;
+  State state_;
 
  public:
   explicit DataFlow(const llvm::Function& f);
