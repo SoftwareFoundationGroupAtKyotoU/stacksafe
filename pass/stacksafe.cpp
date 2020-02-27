3b329b9d06ad8da557d8fa32f9b6c0a43ed4445b
hatsusato <knightspm5@gmail.com>
2020-02-27 16:34:12 +0900

    Add StackSafe

--- pass/stacksafe.cpp
+++ pass/stacksafe.cpp
@@ -1,1 +1,8 @@
 #include "stacksafe.hpp"
+
+namespace stacksafe {
+
+char StackSafe::ID = 0;
+StackSafe::StackSafe() : llvm::ModulePass{ID} {}
+
+}  // namespace stacksafe
--- pass/stacksafe.hpp
+++ pass/stacksafe.hpp
@@ -1,4 +1,16 @@
 #ifndef INCLUDE_GUARD_4EF1A249_1905_4F41_BCD6_F1F1D615A441
 #define INCLUDE_GUARD_4EF1A249_1905_4F41_BCD6_F1F1D615A441
 
+#include <llvm/Pass.h>
+
+namespace stacksafe {
+
+class StackSafe : public ::llvm::ModulePass {
+ public:
+  static char ID;
+  StackSafe();
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_4EF1A249_1905_4F41_BCD6_F1F1D615A441
