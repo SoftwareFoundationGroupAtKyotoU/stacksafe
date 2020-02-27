252b9a18ab0eb8291a8e737a0d624df36b25ce0e
hatsusato <knightspm5@gmail.com>
2020-02-27 11:22:19 +0900

    Add DataFlow

--- src/dataflow.cpp
+++ src/dataflow.cpp
@@ -1,1 +1,7 @@
 #include "dataflow.hpp"
+
+namespace stacksafe {
+
+DataFlow::DataFlow(const llvm::Function& f) : func_{f} {}
+
+}  // namespace stacksafe
--- src/dataflow.hpp
+++ src/dataflow.hpp
@@ -1,4 +1,19 @@
 #ifndef INCLUDE_GUARD_25E5557B_880B_4E38_B068_6B33D855AA6E
 #define INCLUDE_GUARD_25E5557B_880B_4E38_B068_6B33D855AA6E
 
+namespace llvm {
+class Function;
+}
+
+namespace stacksafe {
+
+class DataFlow {
+  const llvm::Function& func_;
+
+ public:
+  explicit DataFlow(const llvm::Function& f);
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_25E5557B_880B_4E38_B068_6B33D855AA6E
