2caa8d0c2c6ca271100a3fe22772b21449d0c835
hatsusato <knightspm5@gmail.com>
2020-02-27 20:17:39 +0900

    Add Value

--- src/value.hpp
+++ src/value.hpp
@@ -1,4 +1,17 @@
 #ifndef INCLUDE_GUARD_2B683EB3_DF38_441C_9ED1_AE57D7DAB1A3
 #define INCLUDE_GUARD_2B683EB3_DF38_441C_9ED1_AE57D7DAB1A3
 
+namespace stacksafe {
+
+template <typename T>
+class Value {
+  using Ptr = const T*;
+  Ptr ptr_;
+
+ public:
+  explicit Value(Ptr p) : ptr_{p} {}
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_2B683EB3_DF38_441C_9ED1_AE57D7DAB1A3
