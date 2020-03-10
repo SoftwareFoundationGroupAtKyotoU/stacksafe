146e18f42f34137c0549b6f315dc01c9873711bd
hatsusato <knightspm5@gmail.com>
2020-03-10 17:41:39 +0900

    Add Node

--- src/tree.hpp
+++ src/tree.hpp
@@ -1,4 +1,18 @@
 #ifndef INCLUDE_GUARD_6F2C02D5_616D_48CD_8A43_B837E2ADE127
 #define INCLUDE_GUARD_6F2C02D5_616D_48CD_8A43_B837E2ADE127
 
+#include <memory>
+
+namespace tree {
+
+class Node {
+  using Ptr = std::shared_ptr<const Node>;
+  Ptr left_, right_;
+  bool black_;
+  int rank_, size_;
+  int value_;
+};
+
+}  // namespace tree
+
 #endif  // INCLUDE_GUARD_6F2C02D5_616D_48CD_8A43_B837E2ADE127
