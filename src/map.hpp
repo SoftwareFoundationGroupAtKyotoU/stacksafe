a75ebeff7ea61a2b4e88fb433b479ff2ee51b7bf
hatsusato <knightspm5@gmail.com>
2020-03-16 22:43:46 +0900

    Add Map

--- src/map.hpp
+++ src/map.hpp
@@ -1,4 +1,14 @@
 #ifndef INCLUDE_GUARD_B24913BE_A7FB_43A0_9D42_907C04FE896D
 #define INCLUDE_GUARD_B24913BE_A7FB_43A0_9D42_907C04FE896D
 
+#include "tree.hpp"
+
+namespace stacksafe {
+
+class Map {
+  TreePtr tree_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_B24913BE_A7FB_43A0_9D42_907C04FE896D
