68f76853621c2cfa191ea3bd54c3b93b23f65d38
hatsusato <knightspm5@gmail.com>
2020-03-29 12:25:36 +0900

    Add Graph

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,4 +1,18 @@
 #ifndef INCLUDE_GUARD_E1E5ACD3_3435_4167_A6B2_0E8D6A2A38AC
 #define INCLUDE_GUARD_E1E5ACD3_3435_4167_A6B2_0E8D6A2A38AC
 
+#include "node.hpp"
+#include "tree.hpp"
+
+namespace stacksafe {
+
+class Graph {
+  using TreeType = Tree<Node, Node>;
+  using PairType = typename TreeType::PairType;
+  using TreePtr = typename TreeType::Ptr;
+  TreePtr tree_;
+};
+
+}  // namespace stacksafe
+
 #endif  // INCLUDE_GUARD_E1E5ACD3_3435_4167_A6B2_0E8D6A2A38AC
