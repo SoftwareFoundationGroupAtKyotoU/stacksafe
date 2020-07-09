54c5514743ec51cb2d6eaf72167831f87e9d1b81
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-09 18:44:17 +0900

    Add Cell

--- src/cell.cpp
+++ src/cell.cpp
@@ -1,1 +1,6 @@
 #include "cell.hpp"
+
+namespace dataflow {
+Cell::Cell(const llvm::Value* value, int level)
+    : value_{value}, level_{level} {}
+}  // namespace dataflow
--- src/cell.hpp
+++ src/cell.hpp
@@ -1,3 +1,16 @@
 #ifndef INCLUDE_GUARD_0B4B9978_D346_4DE5_B632_5DCD571D3B77
 #define INCLUDE_GUARD_0B4B9978_D346_4DE5_B632_5DCD571D3B77
+
+namespace llvm {
+class Value;
+}
+
+namespace dataflow {
+class Cell {
+  const llvm::Value* value_;
+  int level_;
+  Cell(const llvm::Value* value, int level);
+};
+}  // namespace dataflow
+
 #endif  // INCLUDE_GUARD_0B4B9978_D346_4DE5_B632_5DCD571D3B77
