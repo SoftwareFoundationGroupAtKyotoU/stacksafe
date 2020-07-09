f9a48432ef01455323918a7bc6a15c28d75d643d
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-09 14:00:45 +0900

    Add DataFlow

--- pass/dataflow.cpp
+++ pass/dataflow.cpp
@@ -1,1 +1,15 @@
 #include "dataflow.hpp"
+
+namespace dataflow {
+namespace {
+using RegisterPass = llvm::RegisterPass<DataFlow>;
+static RegisterPass dataflow{"dataflow", "dataflow", true, true};
+}  // namespace
+
+char DataFlow::ID = 0;
+DataFlow::DataFlow() : llvm::ModulePass{ID} {}
+bool DataFlow::runOnModule(llvm::Module&) {
+  return false;
+}
+void DataFlow::print(llvm::raw_ostream&, const llvm::Module*) const {}
+}  // namespace dataflow
--- pass/dataflow.hpp
+++ pass/dataflow.hpp
@@ -1,3 +1,16 @@
 #ifndef INCLUDE_GUARD_D8F6EC4F_4011_435A_AD3A_CF631A629063
 #define INCLUDE_GUARD_D8F6EC4F_4011_435A_AD3A_CF631A629063
+
+#include <llvm/Pass.h>
+
+namespace dataflow {
+class DataFlow : public ::llvm::ModulePass {
+ public:
+  static char ID;
+  DataFlow();
+  bool runOnModule(llvm::Module& m) override;
+  void print(llvm::raw_ostream& os, const llvm::Module*) const override;
+};
+}  // namespace dataflow
+
 #endif  // INCLUDE_GUARD_D8F6EC4F_4011_435A_AD3A_CF631A629063
