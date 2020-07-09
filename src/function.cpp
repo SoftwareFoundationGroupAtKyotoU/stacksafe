c8e37643b0b5f3da775173ba399eef3be35029b2
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-09 15:07:22 +0900

    Add function::Scc

--- src/function.cpp
+++ src/function.cpp
@@ -1,1 +1,47 @@
 #include "function.hpp"
+#include <llvm/IR/InstrTypes.h>
+#include <llvm/IR/Module.h>
+#include <memory>
+
+namespace dataflow {
+namespace function {
+auto Scc::solve(const llvm::Module& m) -> std::vector<Component> {
+  auto tarjan = std::make_unique<Scc>();
+  for (const auto& f : m) {
+    if (!f.isDeclaration()) {
+      tarjan->push_back(&f);
+    }
+  }
+  tarjan->run();
+  const auto& result = tarjan->result();
+  std::vector<Component> vec;
+  for (const auto& v : result) {
+    Component c;
+    for (auto p = v.crbegin(); p != v.crend(); ++p) {
+      if (auto f = llvm::dyn_cast<llvm::Function>(*p)) {
+        c.push_back(f);
+      }
+    }
+    vec.push_back(std::move(c));
+  }
+  return vec;
+}
+auto Scc::successors(Ptr p) const -> std::vector<Ptr> {
+  std::vector<Ptr> vec;
+  if (auto f = llvm::dyn_cast<llvm::Function>(p)) {
+    for (const auto& b : *f) {
+      for (const auto& i : b) {
+        if (auto c = llvm::dyn_cast<llvm::CallBase>(&i)) {
+          if (auto d = c->getCalledFunction()) {
+            if (!d->isDeclaration()) {
+              vec.push_back(d);
+            }
+          }
+        }
+      }
+    }
+  }
+  return vec;
+}
+}  // namespace function
+}  // namespace dataflow
--- src/function.hpp
+++ src/function.hpp
@@ -1,3 +1,26 @@
 #ifndef INCLUDE_GUARD_6584D3A4_3AC2_4F36_AE36_A47C2A982896
 #define INCLUDE_GUARD_6584D3A4_3AC2_4F36_AE36_A47C2A982896
+
+#include <vector>
+#include "tarjan.hpp"
+
+namespace llvm {
+class Function;
+class Module;
+}  // namespace llvm
+
+namespace dataflow {
+namespace function {
+class Scc : public stacksafe::tarjan::Solver {
+  using Component = std::vector<const llvm::Function*>;
+
+ public:
+  static std::vector<Component> solve(const llvm::Module& m);
+
+ private:
+  std::vector<Ptr> successors(Ptr f) const override;
+};
+}  // namespace function
+}  // namespace dataflow
+
 #endif  // INCLUDE_GUARD_6584D3A4_3AC2_4F36_AE36_A47C2A982896
