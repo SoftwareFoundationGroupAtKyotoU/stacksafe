fc96bdd81ac7efc0e9b04870c8f81f585cf8d10e
hatsusato <2765630+hatsusato@users.noreply.github.com>
2020-07-09 15:23:56 +0900

    Add Transfer

--- src/transfer.cpp
+++ src/transfer.cpp
@@ -1,1 +1,8 @@
 #include "transfer.hpp"
+#include "utility.hpp"
+
+namespace dataflow {
+auto Transfer::visitInstruction(llvm::Instruction &i) -> RetTy {
+  debug::print("unsupported instruction: " + debug::to_str(i));
+}
+}  // namespace dataflow
--- src/transfer.hpp
+++ src/transfer.hpp
@@ -1,3 +1,16 @@
 #ifndef INCLUDE_GUARD_ECA0C9FA_827B_4E7E_8CE9_9A0F858FBF7F
 #define INCLUDE_GUARD_ECA0C9FA_827B_4E7E_8CE9_9A0F858FBF7F
+
+#include <llvm/IR/InstVisitor.h>
+
+namespace dataflow {
+class Transfer : public llvm::InstVisitor<Transfer, void> {
+  using RetTy = void;
+  using Super = llvm::InstVisitor<Transfer, RetTy>;
+
+ public:
+  RetTy visitInstruction(llvm::Instruction &i);
+};
+}  // namespace dataflow
+
 #endif  // INCLUDE_GUARD_ECA0C9FA_827B_4E7E_8CE9_9A0F858FBF7F
